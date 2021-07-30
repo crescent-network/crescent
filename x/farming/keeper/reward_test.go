4927c7869e0e0b7918740fa5e1a7b3250d509a5f
Hanjun Kim <hallazzang@gmail.com>
2021-07-30 17:12:39 +0900

    test: add test for DistributeRewards

--- x/farming/keeper/reward_test.go
+++ x/farming/keeper/reward_test.go
@@ -11,14 +11,26 @@ import (
 func (suite *KeeperTestSuite) TestDistributionInfos() {
 	normalPlans := []types.PlanI{
 		types.NewFixedAmountPlan(
-			types.NewBasePlan(1, "", types.PlanTypePrivate, suite.addrs[0].String(), suite.addrs[0].String(),
+			types.NewBasePlan(
+				1,
+				"",
+				types.PlanTypePrivate,
+				suite.addrs[0].String(),
+				suite.addrs[0].String(),
 				sdk.NewDecCoins(sdk.NewDecCoinFromDec(denom1, sdk.NewDec(1))),
-				mustParseRFC3339("2021-07-27T00:00:00Z"), mustParseRFC3339("2021-07-28T00:00:00Z")),
+				mustParseRFC3339("2021-07-27T00:00:00Z"),
+				mustParseRFC3339("2021-07-28T00:00:00Z")),
 			sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))),
 		types.NewFixedAmountPlan(
-			types.NewBasePlan(2, "", types.PlanTypePrivate, suite.addrs[0].String(), suite.addrs[0].String(),
+			types.NewBasePlan(
+				2,
+				"",
+				types.PlanTypePrivate,
+				suite.addrs[0].String(),
+				suite.addrs[0].String(),
 				sdk.NewDecCoins(sdk.NewDecCoinFromDec(denom1, sdk.NewDec(1))),
-				mustParseRFC3339("2021-07-27T12:00:00Z"), mustParseRFC3339("2021-07-28T12:00:00Z")),
+				mustParseRFC3339("2021-07-27T12:00:00Z"),
+				mustParseRFC3339("2021-07-28T12:00:00Z")),
 			sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))),
 	}
 
@@ -26,15 +38,21 @@ func (suite *KeeperTestSuite) TestDistributionInfos() {
 		name      string
 		plans     []types.PlanI
 		t         time.Time
-		distrAmts map[uint64]sdk.Coins
+		distrAmts map[uint64]sdk.Coins // planID => sdk.Coins
 	}{
 		{
 			"insufficient farming pool balances",
 			[]types.PlanI{
 				types.NewFixedAmountPlan(
-					types.NewBasePlan(1, "", types.PlanTypePrivate, suite.addrs[0].String(), suite.addrs[0].String(),
+					types.NewBasePlan(
+						1,
+						"",
+						types.PlanTypePrivate,
+						suite.addrs[0].String(),
+						suite.addrs[0].String(),
 						sdk.NewDecCoins(sdk.NewDecCoinFromDec(denom1, sdk.NewDec(1))),
-						mustParseRFC3339("2021-07-27T00:00:00Z"), mustParseRFC3339("2021-07-30T00:00:00Z")),
+						mustParseRFC3339("2021-07-27T00:00:00Z"),
+						mustParseRFC3339("2021-07-30T00:00:00Z")),
 					sdk.NewCoins(sdk.NewInt64Coin(denom3, 10_000_000_000))),
 			},
 			mustParseRFC3339("2021-07-28T00:00:00Z"),
@@ -62,13 +80,17 @@ func (suite *KeeperTestSuite) TestDistributionInfos() {
 			"start time & end time edgecase #4",
 			normalPlans,
 			mustParseRFC3339("2021-07-27T12:00:00Z"),
-			map[uint64]sdk.Coins{1: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000)), 2: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))},
+			map[uint64]sdk.Coins{
+				1: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000)),
+				2: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))},
 		},
 		{
 			"start time & end time edgecase #5",
 			normalPlans,
 			mustParseRFC3339("2021-07-27T23:59:59Z"),
-			map[uint64]sdk.Coins{1: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000)), 2: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))},
+			map[uint64]sdk.Coins{
+				1: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000)),
+				2: sdk.NewCoins(sdk.NewInt64Coin(denom3, 1000))},
 		},
 		{
 			"start time & end time edgecase #6",
@@ -109,5 +131,48 @@ func (suite *KeeperTestSuite) TestDistributionInfos() {
 }
 
 func (suite *KeeperTestSuite) TestDistributeRewards() {
+	plans := []types.PlanI{
+		types.NewFixedAmountPlan(
+			types.NewBasePlan(
+				1,
+				"",
+				types.PlanTypePrivate,
+				suite.addrs[0].String(),
+				suite.addrs[0].String(),
+				sdk.NewDecCoins(
+					sdk.NewDecCoinFromDec(denom1, sdk.NewDecWithPrec(3, 1)),
+					sdk.NewDecCoinFromDec(denom2, sdk.NewDecWithPrec(7, 1))),
+				mustParseRFC3339("2021-07-30T00:00:00Z"),
+				mustParseRFC3339("2021-08-30T00:00:00Z")),
+			sdk.NewCoins(sdk.NewInt64Coin(denom3, 1_000_000))),
+	}
+	for _, plan := range plans {
+		suite.keeper.SetPlan(suite.ctx, plan)
+	}
 
+	suite.Stake(suite.addrs[1], sdk.NewCoins(
+		sdk.NewInt64Coin(denom1, 1_000_000),
+		sdk.NewInt64Coin(denom2, 1_000_000)))
+	suite.keeper.ProcessQueuedCoins(suite.ctx)
+
+	suite.ctx = suite.ctx.WithBlockTime(mustParseRFC3339("2021-07-31T00:00:00Z"))
+	err := suite.keeper.DistributeRewards(suite.ctx)
+	suite.Require().NoError(err)
+
+	rewards := suite.keeper.GetRewardsByFarmer(suite.ctx, suite.addrs[1])
+	suite.Require().Len(rewards, 2)
+	for _, reward := range rewards {
+		switch reward.StakingCoinDenom {
+		case denom1:
+			suite.Require().True(intEq(sdk.NewInt(300_000), reward.RewardCoins.AmountOf(denom3)))
+		case denom2:
+			suite.Require().True(intEq(sdk.NewInt(700_000), reward.RewardCoins.AmountOf(denom3)))
+		}
+	}
+
+	totalRewardCoins := suite.keeper.GetTotalDistributedRewardCoins(suite.ctx, 1)
+	suite.Require().True(coinsEq(sdk.NewCoins(sdk.NewInt64Coin(denom3, 1_000_000)), totalRewardCoins))
+	lastDistributedAt, found := suite.keeper.GetLastDistributedTime(suite.ctx, 1)
+	suite.Require().True(found)
+	suite.Require().Equal(suite.ctx.BlockTime(), lastDistributedAt)
 }
