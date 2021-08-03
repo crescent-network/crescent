1f1d82fe484e4e06d574c2fb9ecc2d5675829bd8
kogisin <jaybkim2@gmail.com>
2021-08-03 16:51:32 +0900

    feat: add skeleton for cli integration tests

---
+++ x/farming/client/cli/cli_test.go
@@ -0,0 +1,84 @@
+package cli_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	farmingtestutil "github.com/tendermint/farming/x/farming/client/testutil"
+	farmingtypes "github.com/tendermint/farming/x/farming/types"
+	tmdb "github.com/tendermint/tm-db"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	cfg     network.Config
+	network *network.Network
+
+	db *tmdb.MemDB // in-memory database is needed for exporting genesis cli integration test
+}
+
+// SetupTest creates a new network for _each_ integration test. We create a new
+// network for each test because there are some state modifications that are
+// needed to be made in order to make useful queries. However, we don't want
+// these state changes to be present in other tests.
+func (s *IntegrationTestSuite) SetupTest() {
+	s.T().Log("setting up integration test suite")
+
+	db := tmdb.NewMemDB()
+
+	cfg := farmingtestutil.NewConfig(db)
+	cfg.NumValidators = 1
+
+	var genesisState farmingtypes.GenesisState
+	err := cfg.Codec.UnmarshalJSON(cfg.GenesisState[farmingtypes.ModuleName], &genesisState)
+	s.Require().NoError(err)
+
+	genesisState.Params = farmingtypes.DefaultParams()
+
+	cfg.GenesisState[farmingtypes.ModuleName] = cfg.Codec.MustMarshalJSON(&genesisState)
+	cfg.AccountTokens = sdk.NewInt(100_000_000_000) // node0token denom
+	cfg.StakingTokens = sdk.NewInt(100_000_000_000) // stake denom
+
+	s.cfg = cfg
+	s.network = network.New(s.T(), cfg)
+	s.db = db
+
+	_, err = s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+}
+
+// TearDownTest cleans up the curret test network after each test in the suite.
+func (s *IntegrationTestSuite) TearDownTest() {
+	s.T().Log("tearing down integration test suite")
+	s.network.Cleanup()
+}
+
+// TestIntegrationTestSuite every integration test suite.
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
+	// TODO: not implemented yet
+}
+
+func (s *IntegrationTestSuite) TestNewCreateRatioPlanCmd() {
+	// TODO: not implemented yet
+}
+
+func (s *IntegrationTestSuite) TestNewStakeCmd() {
+	// TODO: not implemented yet
+}
+
+func (s *IntegrationTestSuite) TestNewUnstakeCmd() {
+	// TODO: not implemented yet
+}
+
+func (s *IntegrationTestSuite) TestNewHarvestCmd() {
+	// TODO: not implemented yet
+}
---
+++ x/farming/client/testutil/cli_helpers.go
@@ -0,0 +1,65 @@
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/simapp/params"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/testutil"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	farmingapp "github.com/tendermint/farming/app"
+	farmingcli "github.com/tendermint/farming/x/farming/client/cli"
+	dbm "github.com/tendermint/tm-db"
+)
+
+// NewConfig returns config that defines the necessary testing requirements
+// used to bootstrap and start an in-process local testing network.
+func NewConfig(dbm *dbm.MemDB) network.Config {
+	encCfg := simapp.MakeTestEncodingConfig()
+
+	cfg := network.DefaultConfig()
+	cfg.AppConstructor = NewAppConstructor(encCfg, dbm)                  // the ABCI application constructor
+	cfg.GenesisState = farmingapp.ModuleBasics.DefaultGenesis(cfg.Codec) // farming genesis state to provide
+	return cfg
+}
+
+// NewAppConstructor returns a new network AppConstructor.
+func NewAppConstructor(encodingCfg params.EncodingConfig, db *dbm.MemDB) network.AppConstructor {
+	return func(val network.Validator) servertypes.Application {
+		return farmingapp.NewFarmingApp(
+			val.Ctx.Logger, db, nil, true, make(map[int64]bool), val.Ctx.Config.RootDir, 0,
+			farmingapp.MakeEncodingConfig(),
+			simapp.EmptyAppOptions{},
+			baseapp.SetPruning(storetypes.NewPruningOptionsFromString(val.AppConfig.Pruning)),
+			baseapp.SetMinGasPrices(val.AppConfig.MinGasPrices),
+		)
+	}
+}
+
+var commonArgs = []string{
+	fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+	fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+	fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10))).String()),
+}
+
+// TODO: not implmemented yet
+
+// MsgCreateFixedAmountPlanExec creates a transaction for creating a private fixed amount plan.
+func MsgCreateFixedAmountPlanExec(clientCtx client.Context, from string, extraArgs ...string) (testutil.BufferWriter, error) {
+
+	args := append([]string{
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, from),
+	}, commonArgs...)
+
+	args = append(args, commonArgs...)
+
+	return clitestutil.ExecTestCLICmd(clientCtx, farmingcli.NewCreateFixedAmountPlanCmd(), args)
+}
