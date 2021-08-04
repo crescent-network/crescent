c57b956e1b17d94a51ab2e2ac1038246f76f1240
Hanjun Kim <hallazzang@gmail.com>
2021-08-04 16:55:31 +0900

    fix: add farmer query parameter for Stakings query
    also add missing validation checks

--- x/farming/keeper/grpc_query.go
+++ x/farming/keeper/grpc_query.go
@@ -143,32 +143,70 @@ func (k Querier) Stakings(c context.Context, req *types.QueryStakingsRequest) (*
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	if req.StakingCoinDenom != "" {
+		if err := sdk.ValidateDenom(req.StakingCoinDenom); err != nil {
+			return nil, err
+		}
+	}
+
 	ctx := sdk.UnwrapSDKContext(c)
 	store := ctx.KVStore(k.storeKey)
 	var stakings []*types.Staking
 	var pageRes *query.PageResponse
 	var err error
 
-	if req.StakingCoinDenom != "" {
-		storePrefix := types.GetStakingsByStakingCoinDenomIndexKey(req.StakingCoinDenom)
-		indexStore := prefix.NewStore(store, storePrefix)
-		pageRes, err = query.Paginate(indexStore, req.Pagination, func(key, value []byte) error {
-			_, stakingID := types.ParseStakingsByStakingCoinDenomIndexKey(append(storePrefix, key...))
-			staking, _ := k.GetStaking(ctx, stakingID)
-			stakings = append(stakings, &staking)
-			return nil
-		})
+	if req.Farmer != "" {
+		farmerAcc, err := sdk.AccAddressFromBech32(req.Farmer)
+		if err != nil {
+			return nil, err
+		}
+
+		staking, found := k.GetStakingByFarmer(ctx, farmerAcc)
+		if found {
+			found := false
+
+			if req.StakingCoinDenom != "" {
+				for _, denom := range staking.StakingCoinDenoms() {
+					if denom == req.StakingCoinDenom {
+						found = true
+						break
+					}
+				}
+			} else {
+				found = true
+			}
+
+			if found {
+				stakings = append(stakings, &staking)
+			}
+		}
+
+		pageRes = &query.PageResponse{
+			NextKey: nil,
+			Total:   uint64(len(stakings)),
+		}
 	} else {
-		stakingStore := prefix.NewStore(store, types.StakingKeyPrefix)
-		pageRes, err = query.Paginate(stakingStore, req.Pagination, func(key, value []byte) error {
-			var staking types.Staking
-			k.cdc.MustUnmarshal(value, &staking)
-			stakings = append(stakings, &staking)
-			return nil
-		})
-	}
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
+		if req.StakingCoinDenom != "" {
+			storePrefix := types.GetStakingsByStakingCoinDenomIndexKey(req.StakingCoinDenom)
+			indexStore := prefix.NewStore(store, storePrefix)
+			pageRes, err = query.Paginate(indexStore, req.Pagination, func(key, value []byte) error {
+				_, stakingID := types.ParseStakingsByStakingCoinDenomIndexKey(append(storePrefix, key...))
+				staking, _ := k.GetStaking(ctx, stakingID)
+				stakings = append(stakings, &staking)
+				return nil
+			})
+		} else {
+			stakingStore := prefix.NewStore(store, types.StakingKeyPrefix)
+			pageRes, err = query.Paginate(stakingStore, req.Pagination, func(key, value []byte) error {
+				var staking types.Staking
+				k.cdc.MustUnmarshal(value, &staking)
+				stakings = append(stakings, &staking)
+				return nil
+			})
+		}
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
 	}
 
 	return &types.QueryStakingsResponse{Stakings: stakings, Pagination: pageRes}, nil
@@ -177,6 +215,12 @@ func (k Querier) Stakings(c context.Context, req *types.QueryStakingsRequest) (*
 func (k Querier) Rewards(c context.Context, req *types.QueryRewardsRequest) (*types.QueryRewardsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.StakingCoinDenom != "" {
+		if err := sdk.ValidateDenom(req.StakingCoinDenom); err != nil {
+			return nil, err
+		}
 	}
 
 	ctx := sdk.UnwrapSDKContext(c)
