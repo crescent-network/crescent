3164858f1fbe0dd98b156e268947b843d9c0e1d6
kogisin <jaybkim2@gmail.com>
2021-08-11 11:51:39 +0900

    test: add integration cli test for ratio plan, stake, and unstake

--- x/farming/client/cli/cli_test.go
+++ x/farming/client/cli/cli_test.go
@@ -159,7 +159,7 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			false, &sdk.TxResponse{}, 0,
 		},
 		{
-			"name invalid case #1",
+			"invalid name case #1",
 			[]string{
 				testutil.WriteToNewTempFile(s.T(), case2.String()).Name(),
 				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
@@ -170,7 +170,7 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			true, &sdk.TxResponse{}, 0,
 		},
 		{
-			"staking coin weights invalid case #1",
+			"invalid staking coin weights case #1",
 			[]string{
 				testutil.WriteToNewTempFile(s.T(), case3.String()).Name(),
 				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
@@ -181,7 +181,7 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			true, &sdk.TxResponse{}, 0,
 		},
 		{
-			"staking coin weights invalid case #2",
+			"invalid staking coin weights case #2",
 			[]string{
 				testutil.WriteToNewTempFile(s.T(), case4.String()).Name(),
 				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
@@ -192,7 +192,7 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			true, &sdk.TxResponse{}, 0,
 		},
 		{
-			"start time & end time invalid case #1",
+			"invalid start time & end time case #1",
 			[]string{
 				testutil.WriteToNewTempFile(s.T(), case5.String()).Name(),
 				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
@@ -203,7 +203,7 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			true, &sdk.TxResponse{}, 0,
 		},
 		{
-			"epoch amount invalid case #1",
+			"invalid epoch amount case #1",
 			[]string{
 				testutil.WriteToNewTempFile(s.T(), case6.String()).Name(),
 				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
@@ -235,23 +235,377 @@ func (s *IntegrationTestSuite) TestNewCreateFixedAmountPlanCmd() {
 			}
 		})
 	}
-
 }
 
 func (s *IntegrationTestSuite) TestNewCreateRatioPlanCmd() {
-	// TODO: not implemented yet
+	val := s.network.Validators[0]
+
+	name := "test"
+	coinWeights := sdk.NewDecCoins(
+		sdk.DecCoin{
+			Denom:  "poolD35A0CC16EE598F90B044CE296A405BA9C381E38837599D96F2F70C2F02A23A4",
+			Amount: sdk.MustNewDecFromStr("1.0"),
+		},
+	)
+
+	// happy case
+	case1 := cli.PrivateRatioPlanRequest{
+		Name:               name,
+		StakingCoinWeights: coinWeights,
+		StartTime:          mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("0.1"),
+	}
+
+	// invalid name
+	case2 := cli.PrivateRatioPlanRequest{
+		Name: `OVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERMOVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERM
+		OVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERMOVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERM
+		OVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERMOVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERM
+		OVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERMOVERMAXLENGTHOVERMAXLENGTHOVERMAXLENGTHOVERM`,
+		StakingCoinWeights: sdk.NewDecCoins(),
+		StartTime:          mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("0.1"),
+	}
+
+	// invalid staking coin weights
+	case3 := cli.PrivateRatioPlanRequest{
+		Name:               name,
+		StakingCoinWeights: sdk.NewDecCoins(),
+		StartTime:          mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("0.1"),
+	}
+
+	// invalid staking coin weights
+	case4 := cli.PrivateRatioPlanRequest{
+		Name:               name,
+		StakingCoinWeights: sdk.NewDecCoins(sdk.NewDecCoin("poolD35A0CC16EE598F90B044CE296A405BA9C381E38837599D96F2F70C2F02A23A4", sdk.NewInt(2))),
+		StartTime:          mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("0.1"),
+	}
+
+	// invalid start time and end time
+	case5 := cli.PrivateRatioPlanRequest{
+		Name:               name,
+		StakingCoinWeights: coinWeights,
+		StartTime:          mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("0.1"),
+	}
+
+	// invalid epoch ratio
+	case6 := cli.PrivateRatioPlanRequest{
+		Name:               name,
+		StakingCoinWeights: coinWeights,
+		StartTime:          mustParseRFC3339("2021-08-13T00:00:00Z"),
+		EndTime:            mustParseRFC3339("2021-08-06T00:00:00Z"),
+		EpochRatio:         sdk.MustNewDecFromStr("1.1"),
+	}
+
+	testCases := []struct {
+		name         string
+		args         []string
+		expectErr    bool
+		respType     proto.Message
+		expectedCode uint32
+	}{
+		{
+			"valid transaction",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case1.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			false, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid name case #1",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case2.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid staking coin weights case #1",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case3.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid staking coin weights case #2",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case4.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid start time & end time case #1",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case5.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid epoch ratio case #1",
+			[]string{
+				testutil.WriteToNewTempFile(s.T(), case6.String()).Name(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.NewCreateRatioPlanCmd()
+			clientCtx := val.ClientCtx
+
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err, out.String())
+				s.Require().NoError(clientCtx.Codec.UnmarshalJSON(out.Bytes(), tc.respType), out.String())
+
+				txResp := tc.respType.(*sdk.TxResponse)
+				s.Require().Equal(tc.expectedCode, txResp.Code, out.String())
+			}
+		})
+	}
 }
 
 func (s *IntegrationTestSuite) TestNewStakeCmd() {
-	// TODO: not implemented yet
+	val := s.network.Validators[0]
+
+	testCases := []struct {
+		name         string
+		args         []string
+		expectErr    bool
+		respType     proto.Message
+		expectedCode uint32
+	}{
+		{
+			"valid transaction",
+			[]string{
+				sdk.NewCoin("stake", sdk.NewInt(100000)).String(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			false, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid staking coin case #1",
+			[]string{
+				sdk.NewCoin("stake", sdk.NewInt(0)).String(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.NewStakeCmd()
+			clientCtx := val.ClientCtx
+
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err, out.String())
+				s.Require().NoError(clientCtx.Codec.UnmarshalJSON(out.Bytes(), tc.respType), out.String())
+
+				txResp := tc.respType.(*sdk.TxResponse)
+				s.Require().Equal(tc.expectedCode, txResp.Code, out.String())
+			}
+		})
+	}
 }
 
 func (s *IntegrationTestSuite) TestNewUnstakeCmd() {
-	// TODO: not implemented yet
+	val := s.network.Validators[0]
+
+	_, err := farmingtestutil.MsgStakeExec(
+		val.ClientCtx,
+		val.Address.String(),
+		sdk.NewCoin("stake", sdk.NewInt(10_000_000)).String(),
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name         string
+		args         []string
+		expectErr    bool
+		respType     proto.Message
+		expectedCode uint32
+	}{
+		{
+			"valid transaction",
+			[]string{
+				sdk.NewCoin("stake", sdk.NewInt(100000)).String(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			false, &sdk.TxResponse{}, 0,
+		},
+		{
+			"invalid unstaking coin case #1",
+			[]string{
+				sdk.NewCoin("stake", sdk.NewInt(0)).String(),
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+			},
+			true, &sdk.TxResponse{}, 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.NewUnstakeCmd()
+			clientCtx := val.ClientCtx
+
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err, out.String())
+				s.Require().NoError(clientCtx.Codec.UnmarshalJSON(out.Bytes(), tc.respType), out.String())
+
+				txResp := tc.respType.(*sdk.TxResponse)
+				s.Require().Equal(tc.expectedCode, txResp.Code, out.String())
+			}
+		})
+	}
 }
 
 func (s *IntegrationTestSuite) TestNewHarvestCmd() {
-	// TODO: not implemented yet
+	// TODO: not fully implemented yet
+
+	// val := s.network.Validators[0]
+
+	// // create fixed amount plan
+	// req := cli.PrivateFixedPlanRequest{
+	// 	Name:               "test",
+	// 	StakingCoinWeights: sdk.NewDecCoins(sdk.NewDecCoin("stake", sdk.NewInt(1))),
+	// 	StartTime:          mustParseRFC3339("2021-08-06T00:00:00Z"),
+	// 	EndTime:            mustParseRFC3339("2021-08-13T00:00:00Z"),
+	// 	EpochAmount:        sdk.NewCoins(sdk.NewInt64Coin("node0token", 100_000_000)),
+	// }
+
+	// file := testutil.WriteToNewTempFile(s.T(), req.String()).Name()
+
+	// _, err := farmingtestutil.MsgCreateFixedAmountPlanExec(
+	// 	val.ClientCtx,
+	// 	val.Address.String(),
+	// 	file,
+	// )
+	// s.Require().NoError(err)
+
+	// // stake coins
+	// _, err = farmingtestutil.MsgStakeExec(
+	// 	val.ClientCtx,
+	// 	val.Address.String(),
+	// 	sdk.NewCoin("stake", sdk.NewInt(10_000_000)).String(),
+	// )
+	// s.Require().NoError(err)
+
+	// // increase epoch days by 2 days so that reward distribution logic is triggered
+
+	// testCases := []struct {
+	// 	name         string
+	// 	args         []string
+	// 	expectErr    bool
+	// 	respType     proto.Message
+	// 	expectedCode uint32
+	// }{
+	// 	{
+	// 		"valid transaction",
+	// 		[]string{
+	// 			fmt.Sprintf("--%s=%s", cli.FlagStakingCoinDenoms, "stake"),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+	// 			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+	// 		},
+	// 		false, &sdk.TxResponse{}, 0,
+	// 	},
+	// 	{
+	// 		"invalid staking coin denoms case #1",
+	// 		[]string{
+	// 			fmt.Sprintf("--%s=%s", cli.FlagStakingCoinDenoms, ""),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+	// 			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+	// 			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+	// 		},
+	// 		true, &sdk.TxResponse{}, 0,
+	// 	},
+	// }
+
+	// for _, tc := range testCases {
+	// 	tc := tc
+
+	// 	s.Run(tc.name, func() {
+	// 		cmd := cli.NewHarvestCmd()
+	// 		clientCtx := val.ClientCtx
+
+	// 		out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+	// 		fmt.Println("out: ", out)
+	// 		fmt.Println("err: ", err)
+	// 		if tc.expectErr {
+	// 			s.Require().Error(err)
+	// 		} else {
+	// 			s.Require().NoError(err, out.String())
+	// 			s.Require().NoError(clientCtx.Codec.UnmarshalJSON(out.Bytes(), tc.respType), out.String())
+
+	// 			txResp := tc.respType.(*sdk.TxResponse)
+	// 			s.Require().Equal(tc.expectedCode, txResp.Code, out.String())
+	// 		}
+	// 	})
+	// }
 }
 
 func mustParseRFC3339(s string) time.Time {
