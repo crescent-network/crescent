c574fb63e169384bcc789798273b1a1ca7177bdf
kogisin <jaybkim2@gmail.com>
2021-10-29 11:00:00 +0900

    fix: broken tests

--- x/farming/simulation/proposals_test.go
+++ x/farming/simulation/proposals_test.go
@@ -42,8 +42,8 @@ func TestProposalContents(t *testing.T) {
 	require.Equal(t, params.DefaultWeightDeletePublicPlanProposal, w2.DefaultWeight())
 
 	content0 := w0.ContentSimulatorFn()(r, ctx, accounts)
-	require.Equal(t, "wNbeHVIkPZ", content0.GetTitle())
-	require.Equal(t, "ODFRregDTqGNoFBIHxvimmIZwLfFyKUfEWAnNBdtdzDmTPXtpHRGdIbuucfTjOygZsTxPjfweXhSUkMhPjMaxKlMIJMOXcnQfyze", content0.GetDescription())
+	require.Equal(t, "eOcbWwNbeH", content0.GetTitle())
+	require.Equal(t, "AjEdlEWDODFRregDTqGNoFBIHxvimmIZwLfFyKUfEWAnNBdtdzDmTPXtpHRGdIbuucfTjOygZsTxPjfweXhSUkMhPjMaxKlMIJMO", content0.GetDescription())
 	require.Equal(t, "farming", content0.ProposalRoute())
 	require.Equal(t, "PublicPlan", content0.ProposalType())
 
@@ -69,14 +69,14 @@ func TestProposalContents(t *testing.T) {
 	require.NoError(t, err)
 
 	content1 := w1.ContentSimulatorFn()(r, ctx, accounts)
-	require.Equal(t, "RuFwMRYUiK", content1.GetTitle())
-	require.Equal(t, "SGLqwTMcxHfWotDllNkIJPMbXzjDVjPOOjCFuIvTyhXKLyhUScOXvYthRXpPfKwMhptXaxIxgqBoUqzrWbaoLTVpQoottZyPFfNO", content1.GetDescription())
+	require.Equal(t, "OoMioXHRuF", content1.GetTitle())
+	require.Equal(t, "REqrXZSGLqwTMcxHfWotDllNkIJPMbXzjDVjPOOjCFuIvTyhXKLyhUScOXvYthRXpPfKwMhptXaxIxgqBoUqzrWbaoLTVpQoottZ", content1.GetDescription())
 	require.Equal(t, "farming", content1.ProposalRoute())
 	require.Equal(t, "PublicPlan", content1.ProposalType())
 
 	content2 := w2.ContentSimulatorFn()(r, ctx, accounts)
-	require.Equal(t, "SKtPDMEoEQ", content2.GetTitle())
-	require.Equal(t, "nCqFrUGMCRZUGJKTZIGPyldsifauoMnJPLTcDHmilcmahlqOELaAUYDBuzsVywnDQfwRLGIWozYaOAilMBcObErwgTDNGWnwQMUg", content2.GetDescription())
+	require.Equal(t, "wQMUgFFSKt", content2.GetTitle())
+	require.Equal(t, "MwMANGoQwFnCqFrUGMCRZUGJKTZIGPyldsifauoMnJPLTcDHmilcmahlqOELaAUYDBuzsVywnDQfwRLGIWozYaOAilMBcObErwgT", content2.GetDescription())
 	require.Equal(t, "farming", content2.ProposalRoute())
 	require.Equal(t, "PublicPlan", content2.ProposalType())
 }
