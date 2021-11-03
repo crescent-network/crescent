2f3200ae8c3bd9893ba18651409d3e7c5e5f2e36
Hanjun Kim <hallazzang@gmail.com>
2021-11-03 22:40:32 +0900

    fix: declare proper sentinel error

--- x/farming/types/errors.go
+++ x/farming/types/errors.go
@@ -6,13 +6,14 @@ import (
 
 // farming module sentinel errors
 var (
-	ErrInvalidPlanType                = sdkerrors.Register(ModuleName, 2, "invalid plan type")
-	ErrInvalidPlanName                = sdkerrors.Register(ModuleName, 3, "invalid plan name")
-	ErrInvalidPlanEndTime             = sdkerrors.Register(ModuleName, 4, "invalid plan end time")
-	ErrInvalidStakingCoinWeights      = sdkerrors.Register(ModuleName, 5, "invalid staking coin weights")
-	ErrInvalidTotalEpochRatio         = sdkerrors.Register(ModuleName, 6, "invalid total epoch ratio")
-	ErrStakingNotExists               = sdkerrors.Register(ModuleName, 7, "staking not exists")
-	ErrConflictPrivatePlanFarmingPool = sdkerrors.Register(ModuleName, 8, "the address is already in use, please use a different plan name")
-	ErrInvalidStakingReservedAmount   = sdkerrors.Register(ModuleName, 9, "staking reserved amount invariant broken")
-	ErrInvalidRemainingRewardsAmount  = sdkerrors.Register(ModuleName, 10, "remaining rewards amount invariant broken")
+	ErrInvalidPlanType                 = sdkerrors.Register(ModuleName, 2, "invalid plan type")
+	ErrInvalidPlanName                 = sdkerrors.Register(ModuleName, 3, "invalid plan name")
+	ErrInvalidPlanEndTime              = sdkerrors.Register(ModuleName, 4, "invalid plan end time")
+	ErrInvalidStakingCoinWeights       = sdkerrors.Register(ModuleName, 5, "invalid staking coin weights")
+	ErrInvalidTotalEpochRatio          = sdkerrors.Register(ModuleName, 6, "invalid total epoch ratio")
+	ErrStakingNotExists                = sdkerrors.Register(ModuleName, 7, "staking not exists")
+	ErrConflictPrivatePlanFarmingPool  = sdkerrors.Register(ModuleName, 8, "the address is already in use, please use a different plan name")
+	ErrInvalidStakingReservedAmount    = sdkerrors.Register(ModuleName, 9, "staking reserved amount invariant broken")
+	ErrInvalidRemainingRewardsAmount   = sdkerrors.Register(ModuleName, 10, "remaining rewards amount invariant broken")
+	ErrInvalidOutstandingRewardsAmount = sdkerrors.Register(ModuleName, 11, "outstanding rewards amount invariant broken")
 )
