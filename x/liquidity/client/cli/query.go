8898a8feaa0e88f468ee4badd3a4e6aaebd3bd4f
kogisin <jaybkim2@gmail.com>
2022-01-12 20:18:08 +0900

    feat: add skeleton for command line interface

--- x/liquidity/client/cli/query.go
+++ x/liquidity/client/cli/query.go
@@ -1,18 +1,21 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	"github.com/crescent-network/crescent/x/liquidity/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd(queryRoute string) *cobra.Command {
-	// Group liquidity queries under a subcommand
 	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
 		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
@@ -20,6 +23,58 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+
+	cmd.AddCommand(
+		QueryParams(),
+		// QueryPools(),
+		// QueryPool(),
+		// QueryPoolsByPair(),
+		// QueryPoolsByReserveAcc(),
+		// QueryPoolsByPoolCoinDenom(),
+		// QueryPairs(),
+		// QueryPair(),
+		// QueryDepositRequests(),
+		// QueryDepositRequest(),
+		// QueryWithdrawRequests(),
+		// QueryWithdrawRequest(),
+		// QuerySwapRequests(),
+		// QuerySwapRequest(),
+	)
+
+	return cmd
+}
+
+func QueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the current liquidity parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as liquidity parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			resp, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&resp.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
 }
--- x/liquidity/client/cli/tx.go
+++ x/liquidity/client/cli/tx.go
@@ -2,15 +2,18 @@ package cli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	"github.com/crescent-network/crescent/x/liquidity/types"
 )
 
-// GetTxCmd returns the transaction commands for this module
+// GetTxCmd returns the transaction commands for the module
 func GetTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
@@ -19,6 +22,174 @@ func GetTxCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+
+	cmd.AddCommand(
+		NewCreatePoolCmd(),
+		NewDepositBatchCmd(),
+		NewWithdrawBatchCmd(),
+		NewSwapBatchCmd(),
+		NewCancelSwapBatchCmd(),
+	)
+
+	return cmd
+}
+
+func NewCreatePoolCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-pool [x-coin] [y-coin]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Create liquidity pool",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Create liquidity pool with deposit coins of x and y.
+Example:
+$ %s tx %s create-pool 1000000000uatom 50000000000ucsnt --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			fmt.Println(clientCtx)
+
+			// TODO: not implemented yet
+
+			// return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return nil
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func NewDepositBatchCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deposit [pool-id] [x-coin] [y-coin]",
+		Args:  cobra.ExactArgs(3),
+		Short: "Deposit x and y coins to the liquidity pool",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Deposit x and y coins to the liquidity pool.
+Example:
+$ %s tx %s deposit 1 1000000000uatom 50000000000ucsnt --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			fmt.Println(clientCtx)
+
+			// TODO: not implemented yet
+
+			// return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return nil
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func NewWithdrawBatchCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "withdraw [pool-id] [pool-coin]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Withdraw pool coin from the specified liquidity pool",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Withdraw pool coin from the specified liquidity pool.
+Example:
+$ %s tx %s withdraw 1 10000pool96EF6EA6E5AC828ED87E8D07E7AE2A8180570ADD212117B2DA6F0B75D17A6295 --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			fmt.Println(clientCtx)
+
+			// TODO: not implemented yet
+
+			// return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return nil
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func NewSwapBatchCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "swap [pool-id] [pool-coin]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Swap x coin to y coin from the specified liquidity pool",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Swap x coin to y coin from the specified liquidity pool.
+Example:
+$ %s tx %s swap 1 10000pool96EF6EA6E5AC828ED87E8D07E7AE2A8180570ADD212117B2DA6F0B75D17A6295 --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			fmt.Println(clientCtx)
+
+			// TODO: not implemented yet
+
+			// return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return nil
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func NewCancelSwapBatchCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "cancel-swap [swap-request-id]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Cancel swap request",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Cancel swap request.
+Example:
+$ %s tx %s cancel-swap 1 --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			fmt.Println(clientCtx)
+
+			// TODO: not implemented yet
+
+			// return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return nil
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
 }
