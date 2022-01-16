2919b3221adeeea6051404ddc398830322120aae
Hanjun Kim <hallazzang@gmail.com>
2022-01-16 21:24:45 +0900

    feat: implement InitGenesis and ExportGenesis
    close #50

--- x/liquidity/genesis.go
+++ x/liquidity/genesis.go
@@ -13,17 +13,70 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 	if err := genState.Validate(); err != nil {
 		panic(err)
 	}
-
 	k.SetParams(ctx, genState.Params)
-
-	// TODO: implement this
+	k.SetLastPairId(ctx, genState.LastPairId)
+	k.SetLastPoolId(ctx, genState.LastPoolId)
+	for _, pair := range genState.Pairs {
+		k.SetPair(ctx, pair)
+	}
+	for _, pool := range genState.Pools {
+		k.SetPool(ctx, pool)
+	}
+	for _, req := range genState.DepositRequests {
+		k.SetDepositRequest(ctx, req)
+	}
+	for _, req := range genState.WithdrawRequests {
+		k.SetWithdrawRequest(ctx, req)
+	}
+	for _, req := range genState.SwapRequests {
+		k.SetSwapRequest(ctx, req)
+	}
+	for _, req := range genState.CancelSwapRequests {
+		k.SetCancelSwapRequest(ctx, req)
+	}
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	genesis := types.DefaultGenesis()
-
-	// TODO: implement this
-
-	return genesis
+	var pairs []types.Pair
+	k.IterateAllPairs(ctx, func(pair types.Pair) (stop bool) {
+		pairs = append(pairs, pair)
+		return false
+	})
+	var pools []types.Pool
+	k.IterateAllPools(ctx, func(pool types.Pool) (stop bool) {
+		pools = append(pools, pool)
+		return false
+	})
+	var depositReqs []types.DepositRequest
+	k.IterateAllDepositRequests(ctx, func(req types.DepositRequest) (stop bool) {
+		depositReqs = append(depositReqs, req)
+		return false
+	})
+	var withdrawReqs []types.WithdrawRequest
+	k.IterateAllWithdrawRequests(ctx, func(req types.WithdrawRequest) (stop bool) {
+		withdrawReqs = append(withdrawReqs, req)
+		return false
+	})
+	var swapReqs []types.SwapRequest
+	k.IterateAllSwapRequests(ctx, func(req types.SwapRequest) (stop bool) {
+		swapReqs = append(swapReqs, req)
+		return false
+	})
+	var cancelSwapReqs []types.CancelSwapRequest
+	k.IterateAllCancelSwapRequests(ctx, func(req types.CancelSwapRequest) (stop bool) {
+		cancelSwapReqs = append(cancelSwapReqs, req)
+		return false
+	})
+	return &types.GenesisState{
+		Params:             k.GetParams(ctx),
+		LastPairId:         k.GetLastPairId(ctx),
+		LastPoolId:         k.GetLastPoolId(ctx),
+		Pairs:              pairs,
+		Pools:              pools,
+		DepositRequests:    depositReqs,
+		WithdrawRequests:   withdrawReqs,
+		SwapRequests:       swapReqs,
+		CancelSwapRequests: cancelSwapReqs,
+	}
 }
