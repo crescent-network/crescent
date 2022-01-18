cff76ff127475b04cfb789fded6e833fb238af66
Hanjun Kim <hallazzang@gmail.com>
2022-01-18 20:31:48 +0900

    fix: fix bug in PoolOrderSource
    close #74

--- x/liquidity/keeper/store.go
+++ x/liquidity/keeper/store.go
@@ -300,7 +300,7 @@ func (k Keeper) GetWithdrawRequest(ctx sdk.Context, poolId, id uint64) (state ty
 		return state, false
 	}
 
-	state = types.MustUnmarshaWithdrawRequest(k.cdc, value)
+	state = types.MustUnmarshalWithdrawRequest(k.cdc, value)
 	return state, true
 }
 
@@ -355,7 +355,7 @@ func (k Keeper) GetSwapRequest(ctx sdk.Context, pairId, id uint64) (state types.
 		return state, false
 	}
 
-	state = types.MustUnmarshaSwapRequest(k.cdc, value)
+	state = types.MustUnmarshalSwapRequest(k.cdc, value)
 	return state, true
 }
 
