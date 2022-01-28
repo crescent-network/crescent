8faf79254d6b8534ac8c061eb1a170de50c90571
Hanjun Kim <hallazzang@gmail.com>
2022-01-28 16:26:03 +0900

    fix: execute only not executed requests

--- x/liquidity/keeper/batch.go
+++ x/liquidity/keeper/batch.go
@@ -30,16 +30,20 @@ func (k Keeper) ExecuteRequests(ctx sdk.Context) {
 		panic(err)
 	}
 	if err := k.IterateAllDepositRequests(ctx, func(req types.DepositRequest) (stop bool, err error) {
-		if err := k.ExecuteDepositRequest(ctx, req); err != nil {
-			return false, err
+		if req.Status == types.RequestStatusNotExecuted {
+			if err := k.ExecuteDepositRequest(ctx, req); err != nil {
+				return false, err
+			}
 		}
 		return false, nil
 	}); err != nil {
 		panic(err)
 	}
 	if err := k.IterateAllWithdrawRequests(ctx, func(req types.WithdrawRequest) (stop bool, err error) {
-		if err := k.ExecuteWithdrawRequest(ctx, req); err != nil {
-			return false, err
+		if req.Status == types.RequestStatusNotExecuted {
+			if err := k.ExecuteWithdrawRequest(ctx, req); err != nil {
+				return false, err
+			}
 		}
 		return false, nil
 	}); err != nil {
