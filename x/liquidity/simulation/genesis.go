8bf3b1b67b6dcaa1eee1525051fb39934d18b50a
Hanjun Kim <hallazzang@gmail.com>
2022-02-08 21:29:00 +0900

    feat: implement RandomizedGenState

--- x/liquidity/simulation/genesis.go
+++ x/liquidity/simulation/genesis.go
@@ -5,22 +5,75 @@ package simulation
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 
 	"github.com/cosmosquad-labs/squad/x/liquidity/types"
 )
 
 // Simulation parameter constants.
-const ()
+const (
+	batchSize          = "batch_size"
+	tickPrecision      = "tick_precision"
+	maxPriceLimitRatio = "max_price_limit_ratio"
+	withdrawFeeRate    = "withdraw_fee_rate"
+	maxOrderLifespan   = "max_order_lifespan"
+)
+
+func genBatchSize(r *rand.Rand) uint32 {
+	return uint32(r.Int31n(5) + 1)
+}
+
+func genTickPrecision(r *rand.Rand) uint32 {
+	return uint32(r.Int31n(4))
+}
+
+func genMaxPriceLimitRatio(r *rand.Rand) sdk.Dec {
+	return simtypes.RandomDecAmount(r, sdk.NewDecWithPrec(2, 1))
+}
+
+func genWithdrawFeeRate(r *rand.Rand) sdk.Dec {
+	return simtypes.RandomDecAmount(r, sdk.NewDecWithPrec(1, 2))
+}
+
+func genMaxOrderLifespan(r *rand.Rand) time.Duration {
+	return time.Duration(r.Int63n(int64(72 * time.Hour)))
+}
 
 // RandomizedGenState generates a random GenesisState for liquidity.
 func RandomizedGenState(simState *module.SimulationState) {
-	genesis := types.GenesisState{
-		Params: types.DefaultParams(),
-	}
+	genesis := types.DefaultGenesis()
 
-	bz, _ := json.MarshalIndent(&genesis, "", " ")
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, batchSize, &genesis.Params.BatchSize, simState.Rand,
+		func(r *rand.Rand) { genesis.Params.BatchSize = genBatchSize(r) },
+	)
+
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, tickPrecision, &genesis.Params.TickPrecision, simState.Rand,
+		func(r *rand.Rand) { genesis.Params.TickPrecision = genTickPrecision(r) },
+	)
+
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, maxPriceLimitRatio, &genesis.Params.MaxPriceLimitRatio, simState.Rand,
+		func(r *rand.Rand) { genesis.Params.MaxPriceLimitRatio = genMaxPriceLimitRatio(r) },
+	)
+
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, withdrawFeeRate, &genesis.Params.WithdrawFeeRate, simState.Rand,
+		func(r *rand.Rand) { genesis.Params.WithdrawFeeRate = genWithdrawFeeRate(r) },
+	)
+
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, maxOrderLifespan, &genesis.Params.MaxOrderLifespan, simState.Rand,
+		func(r *rand.Rand) { genesis.Params.MaxOrderLifespan = genMaxOrderLifespan(r) },
+	)
+
+	bz, _ := json.MarshalIndent(genesis, "", " ")
 	fmt.Printf("Selected randomly generated liquidity parameters:\n%s\n", bz)
-	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&genesis)
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(genesis)
 }
