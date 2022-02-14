5e530317c59b5c8a9625440f5eae25517f68fea6
kogisin <jaybkim2@gmail.com>
2022-02-14 16:33:31 +0900

    fix: broken checks

--- x/claim/types/expected_keepers.go
+++ x/claim/types/expected_keepers.go
@@ -2,20 +2,19 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	// authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
 type AccountKeeper interface {
-	GetModuleAddress(name string) sdk.AccAddress
-	SetModuleAccount(ctx sdk.Context, macc authtypes.ModuleAccountI)
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
+	// GetModuleAddress(name string) sdk.AccAddress
+	// SetModuleAccount(ctx sdk.Context, macc authtypes.ModuleAccountI)
+	// GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
 }
 
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromModuleToModule(ctx sdk.Context, senderPool, recipientPool string, amt sdk.Coins) error
-	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
-	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	// GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	// MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 }
