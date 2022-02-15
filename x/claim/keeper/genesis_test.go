d8a35f1ca8698ed94e8047a67189c109cd19beff
kogisin <jaybkim2@gmail.com>
2022-02-16 00:44:48 +0900

    test: adding genesis test cases

--- x/claim/keeper/genesis_test.go
+++ x/claim/keeper/genesis_test.go
@@ -1,5 +1,19 @@
 package keeper_test
 
-func (s *KeeperTestSuite) TestGenesisState() {
+import (
+	"github.com/cosmosquad-labs/squad/x/claim/types"
+
+	_ "github.com/stretchr/testify/suite"
+)
+
+func (s *KeeperTestSuite) TestDefaultGenesis() {
+	genState := types.DefaultGenesis()
+
+	s.keeper.InitGenesis(s.ctx, *genState)
+	// got := s.keeper.ExportGenesis(s.ctx)
+	// s.Require().Equal(genState, got)
+}
+
+func (s *KeeperTestSuite) TestImportExportGenesis() {
 	// TODO: not implemented yet
 }
