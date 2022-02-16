b0f0e1d19ec127f357d85408a6548564626ed73d
kogisin <jaybkim2@gmail.com>
2022-02-16 17:57:47 +0900

    review: add airdrops for the claim record query

--- x/claim/types/query.pb.gw.go
+++ x/claim/types/query.pb.gw.go
@@ -372,7 +372,7 @@ var (
 
 	pattern_Query_Airdrop_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"squad", "claim", "v1beta1", "airdrops", "airdrop_id"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ClaimRecord_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"squad", "claim", "v1beta1", "airdrop_id", "claim_records", "recipient"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_ClaimRecord_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6}, []string{"squad", "claim", "v1beta1", "airdrops", "airdrop_id", "claim_records", "recipient"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
