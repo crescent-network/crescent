057539b9ed690bb7822e0470aa2af42b64334097
Hanjun Kim <hallazzang@gmail.com>
2022-02-18 03:00:41 +0900

    test: add order book internal test

---
+++ x/liquidity/amm/orderbook_internal_test.go
@@ -0,0 +1,53 @@
+package amm
+
+import (
+	"sort"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	squad "github.com/cosmosquad-labs/squad/types"
+)
+
+// Copied from orderbook_test.go
+func newOrder(dir OrderDirection, price sdk.Dec, amt sdk.Int) *BaseOrder {
+	var offerCoinDenom, demandCoinDenom string
+	switch dir {
+	case Buy:
+		offerCoinDenom, demandCoinDenom = "denom2", "denom1"
+	case Sell:
+		offerCoinDenom, demandCoinDenom = "denom1", "denom2"
+	}
+	return NewBaseOrder(dir, price, amt, sdk.NewCoin(offerCoinDenom, OfferCoinAmount(dir, price, amt)), demandCoinDenom)
+}
+
+func TestOrderBookTicks_add(t *testing.T) {
+	prices := []sdk.Dec{
+		squad.ParseDec("1.0"),
+		squad.ParseDec("1.1"),
+		squad.ParseDec("1.05"),
+		squad.ParseDec("1.1"),
+		squad.ParseDec("1.2"),
+		squad.ParseDec("0.9"),
+		squad.ParseDec("0.9"),
+	}
+	var ticks orderBookTicks
+	for _, price := range prices {
+		ticks.add(newOrder(Buy, price, sdk.NewInt(10000)))
+	}
+	pricesSet := map[string]struct{}{}
+	for _, price := range prices {
+		pricesSet[price.String()] = struct{}{}
+	}
+	prices = nil
+	for priceStr := range pricesSet {
+		prices = append(prices, squad.ParseDec(priceStr))
+	}
+	sort.Slice(prices, func(i, j int) bool {
+		return prices[i].GT(prices[j])
+	})
+	for i, price := range prices {
+		require.True(sdk.DecEq(t, price, ticks[i].price))
+	}
+}
