11e43bd64173cae0fc171aa542c4002adfd50afb
dongsam <dongsamb@gmail.com>
2022-02-28 02:46:55 +0900

    fix: refactor mint inflation logic, add blockDuration event filed

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -18,24 +18,28 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	// fetch stored params
 	params := k.GetParams(ctx)
 
-	InflationSchedules := k.GetInflationSchedules(ctx)
+	lastBlockTime := k.GetLastBlockTime(ctx)
+	// if not set LastBlockTime(e.g. fist block), skip minting inflation
+	if lastBlockTime == nil {
+		k.SetLastBlockTime(ctx, ctx.BlockTime())
+		return
+	}
+
+	inflationSchedules := k.GetInflationSchedules(ctx)
 	blockInflation := sdk.ZeroInt()
-	for _, schedule := range InflationSchedules {
+	var blockDurationForInflation time.Duration
+	for _, schedule := range inflationSchedules {
 		if squadtypes.DateRangeIncludes(schedule.StartTime, schedule.EndTime, ctx.BlockTime()) {
-			lastBlockTime := k.GetLastBlockTime(ctx)
-			// if not set LastBlockTime(e.g. fist block), skip minting inflation
-			if lastBlockTime == nil {
-				break
-			}
-			lastBlockTimeDiff := ctx.BlockTime().Sub(*lastBlockTime)
-			if lastBlockTimeDiff > params.BlockTimeThreshold {
-				lastBlockTimeDiff = params.BlockTimeThreshold
+			blockDurationForInflation = ctx.BlockTime().Sub(*lastBlockTime)
+			if blockDurationForInflation > params.BlockTimeThreshold {
+				blockDurationForInflation = params.BlockTimeThreshold
 			}
 			// blockInflation = InflationAmountThisPeriod * min(CurrentBlockTime-LastBlockTime,BlockTimeThreshold)/(InflationPeriodEndDate-InflationPeriodStartDate)
-			blockInflation = schedule.Amount.MulRaw(lastBlockTimeDiff.Nanoseconds()).QuoRaw(schedule.EndTime.Sub(schedule.StartTime).Nanoseconds())
+			blockInflation = schedule.Amount.MulRaw(blockDurationForInflation.Nanoseconds()).QuoRaw(schedule.EndTime.Sub(schedule.StartTime).Nanoseconds())
 			break
 		}
 	}
+
 	if blockInflation.IsPositive() {
 		mintedCoin := sdk.NewCoin(params.MintDenom, blockInflation)
 		mintedCoins := sdk.NewCoins(mintedCoin)
@@ -53,11 +57,11 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 		if mintedCoin.Amount.IsInt64() {
 			defer telemetry.ModuleSetGauge(types.ModuleName, float32(mintedCoin.Amount.Int64()), "minted_tokens")
 		}
-
 		ctx.EventManager().EmitEvent(
 			sdk.NewEvent(
 				types.EventTypeMint,
 				sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
+				sdk.NewAttribute(types.AttributeKeyBlockDuration, blockDurationForInflation.String()),
 			),
 		)
 	}
