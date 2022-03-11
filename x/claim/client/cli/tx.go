c75d3dc039f1d430c48de8064169130cf0381c96
kogisin <jaybkim2@gmail.com>
2022-03-11 17:15:42 +0900

    feat: remove test cli commands

--- x/claim/client/cli/tx.go
+++ x/claim/client/cli/tx.go
@@ -4,20 +4,15 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	"github.com/cosmosquad-labs/squad/x/claim/types"
-	liqtypes "github.com/cosmosquad-labs/squad/x/liquidity/types"
-	lstypes "github.com/cosmosquad-labs/squad/x/liquidstaking/types"
 )
 
 // GetTxCmd returns the transaction commands for the module.
@@ -32,10 +27,6 @@ func GetTxCmd() *cobra.Command {
 
 	cmd.AddCommand(
 		NewClaimCmd(),
-		NewDepositAndClaimCmd(),
-		NewSwapAndClaimCmd(),
-		NewLiquidStakeAndClaimCmd(),
-		NewVoteAndClaimCmd(),
 	)
 
 	return cmd
@@ -86,200 +77,6 @@ $ %s tx %s claim 1 farming --from mykey
 		},
 	}
 
-	flags.AddTxFlagsToCmd(cmd)
-
-	return cmd
-}
-
-func NewDepositAndClaimCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "test-deposit-claim",
-		Args:  cobra.NoArgs,
-		Short: "test-deposit-claim",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Test
-Example
-$ %s tx %s test-deposit-claim --from mykey
-`,
-				version.AppName, types.ModuleName,
-			),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-			recipient := clientCtx.GetFromAddress()
-
-			msgDeposit := liqtypes.NewMsgDeposit(
-				recipient,
-				1,
-				sdk.NewCoins(
-					sdk.NewCoin("uatom", sdk.NewInt(10_000_000)),
-					sdk.NewCoin("uusd", sdk.NewInt(10_000_000)),
-				),
-			)
-
-			msgClaim := types.NewMsgClaim(
-				1,
-				recipient,
-				types.ConditionTypeDeposit,
-			)
-
-			msgs := []sdk.Msg{
-				msgDeposit,
-				msgClaim,
-			}
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
-		},
-	}
-	flags.AddTxFlagsToCmd(cmd)
-
-	return cmd
-}
-
-func NewSwapAndClaimCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "test-swap-claim",
-		Args:  cobra.NoArgs,
-		Short: "test-swap-claim",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Test
-Example
-$ %s tx %s test-swap-claim --from mykey
-`,
-				version.AppName, types.ModuleName,
-			),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			recipient := clientCtx.GetFromAddress()
-			direction := liqtypes.OrderDirectionSell
-			offerCoin := sdk.NewInt64Coin("uatom", 1000)
-			demandCoinDenom := "uusd"
-			price := sdk.MustNewDecFromStr("1.0")
-			amount := sdk.NewInt(1000)
-			orderLifespan := 10 * time.Second
-
-			msgSwap := &liqtypes.MsgLimitOrder{
-				Orderer:         recipient.String(),
-				PairId:          1,
-				Direction:       direction,
-				OfferCoin:       offerCoin,
-				DemandCoinDenom: demandCoinDenom,
-				Price:           price,
-				Amount:          amount,
-				OrderLifespan:   orderLifespan,
-			}
-
-			msgClaim := types.NewMsgClaim(
-				1,
-				recipient,
-				types.ConditionTypeSwap,
-			)
-
-			msgs := []sdk.Msg{
-				msgSwap,
-				msgClaim,
-			}
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
-		},
-	}
-	flags.AddTxFlagsToCmd(cmd)
-
-	return cmd
-}
-
-func NewLiquidStakeAndClaimCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "test-liquidstake-claim",
-		Args:  cobra.NoArgs,
-		Short: "test-liquidstake-claim",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Test
-Example
-$ %s tx %s test-liquidstake-claim --from mykey
-`,
-				version.AppName, types.ModuleName,
-			),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-			recipient := clientCtx.GetFromAddress()
-
-			msgLiquidStake := lstypes.NewMsgLiquidStake(
-				recipient,
-				sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10_000_000)),
-			)
-
-			msgClaim := types.NewMsgClaim(
-				1,
-				recipient,
-				types.ConditionTypeLiquidStake,
-			)
-
-			msgs := []sdk.Msg{
-				msgLiquidStake,
-				msgClaim,
-			}
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
-		},
-	}
-	flags.AddTxFlagsToCmd(cmd)
-
-	return cmd
-}
-
-func NewVoteAndClaimCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "test-vote-claim",
-		Args:  cobra.NoArgs,
-		Short: "test-vote-claim",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Test
-Example
-$ %s tx %s test-vote-claim --from mykey
-`,
-				version.AppName, types.ModuleName,
-			),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-			recipient := clientCtx.GetFromAddress()
-
-			msgVote := govtypes.NewMsgVote(
-				recipient,
-				1,
-				govtypes.OptionYes,
-			)
-
-			msgClaim := types.NewMsgClaim(
-				1,
-				recipient,
-				types.ConditionTypeVote,
-			)
-
-			msgs := []sdk.Msg{
-				msgVote,
-				msgClaim,
-			}
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
-		},
-	}
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
