6453a77df17508909d24f3798151a3f3a517b25f
crypin <crypin@crescent.foundation>
2022-03-16 21:46:11 +0900

    fix: init app/coinfig for app name, bech32Prefix

--- app/config.go
+++ app/config.go
@@ -1,16 +1,16 @@
 package app
 
 const (
-	AppName        = "SquadApp"
-	AppUserHomeDir = ".squadapp"
-	AppBinary      = "squad"
+	AppName        = "CrescentApp"
+	AppUserHomeDir = ".crescent"
+	AppBinary      = "crescentd"
 
 	// BIP-44 path "44'/118'/0'/0/0"
 	Purpose  = uint32(44)
 	CoinType = uint32(118)
 
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = "cosmos"
+	Bech32PrefixAccAddr = "cre"
 	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
 	Bech32PrefixAccPub = Bech32PrefixAccAddr + "pub"
 	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
