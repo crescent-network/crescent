a6c83da48fb82fe0f98d0afaee7e892e70c4e708
crypin <crypin@crescent.foundation>
2022-05-17 13:50:21 +0900

    feat: add v2 upgrade handler

---
+++ app/upgrades/mainnet/v2.0.0/upgrade.go
@@ -0,0 +1,22 @@
+package v2_0_0
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+const UpgradeName = "v2.0.0"
+
+func UpgradeHandler(mm *module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		newVM, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return newVM, err
+		}
+		return newVM, err
+	}
+}
+
+var StoreUpgrades store.StoreUpgrades
