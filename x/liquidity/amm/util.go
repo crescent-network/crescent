95d384fd8bf276c869cacaf5c226ccc164b818bf
Hanjun Kim <hallazzang@gmail.com>
2022-06-10 03:00:10 +0900

    chore: remove outdated code (cherry-picked)
    (cherry picked from commit db9a709fae3a1845da1b51d09e0cf06739c2db4d)

--- x/liquidity/amm/util.go
+++ x/liquidity/amm/util.go
@@ -2,7 +2,6 @@ package amm
 
 import (
 	"fmt"
-	"sort"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -18,11 +17,4 @@ func OfferCoinAmount(dir OrderDirection, price sdk.Dec, amt sdk.Int) sdk.Int {
 	default:
 		panic(fmt.Sprintf("invalid order direction: %s", dir))
 	}
-}
-
-// sortTicks sorts given ticks in descending order.
-func sortTicks(ticks []sdk.Dec) {
-	sort.Slice(ticks, func(i, j int) bool {
-		return ticks[i].GT(ticks[j])
-	})
 }
