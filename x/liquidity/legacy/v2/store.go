f239a66d6b9c7c02827d2dc9e52292cab2c093f1
Hanjun Kim <hallazzang@gmail.com>
2022-06-16 17:52:44 +0900

    feat: add migration
    (cherry picked from commit 3100d4083df8510f1e58dee4d9cd672dd9d56c31)

---
+++ x/liquidity/legacy/v2/store.go
@@ -0,0 +1,52 @@
+package v2
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	v1liquidity "github.com/cosmosquad-labs/squad/x/liquidity/legacy/v1"
+	"github.com/cosmosquad-labs/squad/x/liquidity/types"
+)
+
+func MigratePool(store sdk.KVStore, cdc codec.BinaryCodec) error {
+	iter := sdk.KVStorePrefixIterator(store, types.PoolKeyPrefix)
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		var oldPool v1liquidity.Pool
+		if err := cdc.Unmarshal(iter.Value(), &oldPool); err != nil {
+			return err
+		}
+
+		newPool := types.Pool{
+			Type:                  types.PoolTypeBasic,
+			Id:                    oldPool.Id,
+			PairId:                oldPool.PairId,
+			Creator:               "",
+			ReserveAddress:        oldPool.ReserveAddress,
+			PoolCoinDenom:         oldPool.PoolCoinDenom,
+			MinPrice:              nil,
+			MaxPrice:              nil,
+			TranslationX:          nil,
+			TranslationY:          nil,
+			LastDepositRequestId:  oldPool.LastDepositRequestId,
+			LastWithdrawRequestId: oldPool.LastWithdrawRequestId,
+			Disabled:              oldPool.Disabled,
+		}
+		bz, err := cdc.Marshal(&newPool)
+		if err != nil {
+			return err
+		}
+		store.Set(iter.Key(), bz)
+	}
+
+	return nil
+}
+
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	if err := MigratePool(store, cdc); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ x/liquidity/legacy/v2/store_test.go
@@ -0,0 +1,55 @@
+package v2_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	chain "github.com/cosmosquad-labs/squad/app"
+	utils "github.com/cosmosquad-labs/squad/types"
+	v1liquidity "github.com/cosmosquad-labs/squad/x/liquidity/legacy/v1"
+	v2liquidity "github.com/cosmosquad-labs/squad/x/liquidity/legacy/v2"
+	"github.com/cosmosquad-labs/squad/x/liquidity/types"
+)
+
+func TestMigratePool(t *testing.T) {
+	cdc := chain.MakeTestEncodingConfig().Marshaler
+	storeKey := sdk.NewKVStoreKey("liquidity")
+	ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(storeKey)
+
+	oldPool := v1liquidity.Pool{
+		Id:                    1,
+		PairId:                1,
+		ReserveAddress:        utils.TestAddress(0).String(),
+		PoolCoinDenom:         "pool1",
+		LastDepositRequestId:  2,
+		LastWithdrawRequestId: 3,
+		Disabled:              true,
+	}
+	oldPoolValue := cdc.MustMarshal(&oldPool)
+	key := types.GetPoolKey(oldPool.Id)
+	store.Set(key, oldPoolValue)
+
+	require.NoError(t, v2liquidity.MigrateStore(ctx, storeKey, cdc))
+
+	newPool := types.Pool{
+		Type:                  types.PoolTypeBasic,
+		Id:                    1,
+		PairId:                1,
+		Creator:               "",
+		ReserveAddress:        utils.TestAddress(0).String(),
+		PoolCoinDenom:         "pool1",
+		MinPrice:              nil,
+		MaxPrice:              nil,
+		TranslationX:          nil,
+		TranslationY:          nil,
+		LastDepositRequestId:  2,
+		LastWithdrawRequestId: 3,
+		Disabled:              true,
+	}
+	newPoolValue := cdc.MustMarshal(&newPool)
+	require.Equal(t, newPoolValue, store.Get(key))
+}
