2dd35168add53a39cae7e9bbe8886d63adf8cf5f
Hanjun Kim <hallazzang@gmail.com>
2022-06-20 16:18:34 +0900

    feat: add more param getters
    (cherry picked from commit 48725b803695d95cba84d88faab8b5ef54dbc2e6)

--- x/liquidity/keeper/params.go
+++ x/liquidity/keeper/params.go
@@ -1,21 +1,26 @@
 package keeper
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/crescent-network/crescent/x/liquidity/types"
 )
 
-func (k Keeper) GetMaxPriceLimitRatio(ctx sdk.Context) (ratio sdk.Dec) {
-	k.paramSpace.Get(ctx, types.KeyMaxPriceLimitRatio, &ratio)
+// GetBatchSize returns the current batch size parameter.
+func (k Keeper) GetBatchSize(ctx sdk.Context) (batchSize uint32) {
+	k.paramSpace.Get(ctx, types.KeyBatchSize, &batchSize)
 	return
 }
 
+// GetTickPrecision returns the current tick precision parameter.
 func (k Keeper) GetTickPrecision(ctx sdk.Context) (tickPrec uint32) {
 	k.paramSpace.Get(ctx, types.KeyTickPrecision, &tickPrec)
 	return
 }
 
+// GetFeeCollector returns the current fee collector address parameter.
 func (k Keeper) GetFeeCollector(ctx sdk.Context) sdk.AccAddress {
 	var feeCollectorAddr string
 	k.paramSpace.Get(ctx, types.KeyFeeCollectorAddress, &feeCollectorAddr)
@@ -26,6 +31,7 @@ func (k Keeper) GetFeeCollector(ctx sdk.Context) sdk.AccAddress {
 	return addr
 }
 
+// GetDustCollector returns the current dust collector address parameter.
 func (k Keeper) GetDustCollector(ctx sdk.Context) sdk.AccAddress {
 	var dustCollectorAddr string
 	k.paramSpace.Get(ctx, types.KeyDustCollectorAddress, &dustCollectorAddr)
@@ -36,12 +42,60 @@ func (k Keeper) GetDustCollector(ctx sdk.Context) sdk.AccAddress {
 	return addr
 }
 
+// GetMinInitialPoolCoinSupply returns the current minimum pool coin supply
+// parameter.
 func (k Keeper) GetMinInitialPoolCoinSupply(ctx sdk.Context) (i sdk.Int) {
 	k.paramSpace.Get(ctx, types.KeyMinInitialPoolCoinSupply, &i)
 	return
 }
 
+// GetPairCreationFee returns the current pair creation fee parameter.
+func (k Keeper) GetPairCreationFee(ctx sdk.Context) (fee sdk.Coins) {
+	k.paramSpace.Get(ctx, types.KeyPairCreationFee, &fee)
+	return
+}
+
+// GetPoolCreationFee returns the current pool creation fee parameter.
 func (k Keeper) GetPoolCreationFee(ctx sdk.Context) (fee sdk.Coins) {
 	k.paramSpace.Get(ctx, types.KeyPoolCreationFee, &fee)
+	return
+}
+
+// GetMinInitialDepositAmount returns the current minimum initial deposit
+// amount parameter.
+func (k Keeper) GetMinInitialDepositAmount(ctx sdk.Context) (amt sdk.Int) {
+	k.paramSpace.Get(ctx, types.KeyMinInitialDepositAmount, &amt)
+	return
+}
+
+// GetMaxPriceLimitRatio returns the current maximum price limit ratio
+// parameter.
+func (k Keeper) GetMaxPriceLimitRatio(ctx sdk.Context) (ratio sdk.Dec) {
+	k.paramSpace.Get(ctx, types.KeyMaxPriceLimitRatio, &ratio)
+	return
+}
+
+// GetMaxOrderLifespan returns the current maximum order lifespan
+// parameter.
+func (k Keeper) GetMaxOrderLifespan(ctx sdk.Context) (maxLifespan time.Duration) {
+	k.paramSpace.Get(ctx, types.KeyMaxOrderLifespan, &maxLifespan)
+	return
+}
+
+// GetDepositExtraGas returns the current deposit extra gas parameter.
+func (k Keeper) GetDepositExtraGas(ctx sdk.Context) (gas sdk.Gas) {
+	k.paramSpace.Get(ctx, types.KeyDepositExtraGas, &gas)
+	return
+}
+
+// GetWithdrawExtraGas returns the current withdraw extra gas parameter.
+func (k Keeper) GetWithdrawExtraGas(ctx sdk.Context) (gas sdk.Gas) {
+	k.paramSpace.Get(ctx, types.KeyWithdrawExtraGas, &gas)
+	return
+}
+
+// GetOrderExtraGas returns the current order extra gas parameter.
+func (k Keeper) GetOrderExtraGas(ctx sdk.Context) (gas sdk.Gas) {
+	k.paramSpace.Get(ctx, types.KeyOrderExtraGas, &gas)
 	return
 }
---
+++ x/liquidity/keeper/params_test.go
@@ -0,0 +1,57 @@
+package keeper_test
+
+import (
+	"github.com/cosmosquad-labs/squad/x/liquidity/types"
+)
+
+func (s *KeeperTestSuite) TestGetBatchSize() {
+	s.Require().EqualValues(types.DefaultBatchSize, s.keeper.GetBatchSize(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetTickPrecision() {
+	s.Require().EqualValues(types.DefaultTickPrecision, s.keeper.GetTickPrecision(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetFeeCollector() {
+	s.Require().EqualValues(types.DefaultFeeCollectorAddress, s.keeper.GetFeeCollector(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetDustCollector() {
+	s.Require().EqualValues(types.DefaultDustCollectorAddress, s.keeper.GetDustCollector(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetMinInitialPoolCoinSupply() {
+	s.Require().EqualValues(types.DefaultMinInitialPoolCoinSupply, s.keeper.GetMinInitialPoolCoinSupply(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetPairCreationFee() {
+	s.Require().EqualValues(types.DefaultPairCreationFee, s.keeper.GetPairCreationFee(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetPoolCreationFee() {
+	s.Require().EqualValues(types.DefaultPoolCreationFee, s.keeper.GetPoolCreationFee(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetMinInitialDepositAmount() {
+	s.Require().EqualValues(types.DefaultMinInitialDepositAmount, s.keeper.GetMinInitialDepositAmount(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetMaxPriceLimitRatio() {
+	s.Require().EqualValues(types.DefaultMaxPriceLimitRatio, s.keeper.GetMaxPriceLimitRatio(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetMaxOrderLifespan() {
+	s.Require().EqualValues(types.DefaultMaxOrderLifespan, s.keeper.GetMaxOrderLifespan(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetDepositExtraGas() {
+	s.Require().EqualValues(types.DefaultDepositExtraGas, s.keeper.GetDepositExtraGas(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetWithdrawExtraGas() {
+	s.Require().EqualValues(types.DefaultWithdrawExtraGas, s.keeper.GetWithdrawExtraGas(s.ctx))
+}
+
+func (s *KeeperTestSuite) TestGetOrderExtraGas() {
+	s.Require().EqualValues(types.DefaultOrderExtraGas, s.keeper.GetOrderExtraGas(s.ctx))
+}
