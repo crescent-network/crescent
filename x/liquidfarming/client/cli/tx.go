jeonghoyeo7/squad

feat: apply updated auction design (#374)
* feat: add new store prefix key and working on begin blocker

* feat: working on design changes

* feat: use time.duration for RewardsAuctionDuration and update begin blocker

* test: add test cases for begin blocker

* chore: update LiquidFarm and LiquidUnfarm and update test cases

* chore: update store key and prefixes, update spec docs, and deducting fees

* refactor: compound rewards and deduct fees logic

* docs: fix WinningBid to Bid

* chore: fix to move funds from withdrawnReserveAddr to liquidfarmReserveAddr and refactor FinishRewardsAuction

* feat: send remaining coins to fee collector

* chore: refactoring abci logic and updating test cases

* chore: fix logic when liquid farm is removed in params

* test: add more edge cases

* env: update config files to be compatible for the latest ignite

* build: proto-all

* docs: add more context for prerequisites

* feat: add AdvanceAuction

* feat: adding new grpc queries and remove mint-rate and burn-rate queries

* chore: remove unused code and update AdvanceAuction logic

* env: add addresses in config-test.yml

* chore: add grpc test cases and add rewards auction param string status

* docs: update api and cli docs

* test: increase coverage

* review: apply suggestions

x/liquidfarming/client/cli/tx.go
@@ -13,6 +13,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 
+	"github.com/cosmosquad-labs/squad/v3/x/liquidfarming/keeper"
 	"github.com/cosmosquad-labs/squad/v3/x/liquidfarming/types"
 )
 
@@ -33,6 +34,10 @@ func GetTxCmd() *cobra.Command {
 		NewPlaceBidCmd(),
 		NewRefundBidCmd(),
 	)
+
+	if keeper.EnableAdvanceAuction {
+		cmd.AddCommand(NewAdvanceAuctionCmd())
+	}
 
 	return cmd
 
@@ -266,6 +271,41 @@ $ %s tx %s refund-bid 1 1 --from mykey
 				poolId,
 				clientCtx.GetFromAddress().String(),
 			)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// NewAdvanceAuctionCmd implements the advance auction by 1 command handler.
+func NewAdvanceAuctionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "advance-auction",
+		Args:  cobra.NoArgs,
+		Short: "Advance auction by 1 to simulate rewards auction",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Advance auction by 1 to simulate rewards auction.
+This message is available for testing purpose and it can only be enabled when you build the binary with "make install-testing" command. 
+
+Example:
+$ %s tx %s advance-auction --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			requesterAcc := clientCtx.GetFromAddress()
+
+			msg := types.NewMsgAdvanceAuction(requesterAcc)
 
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},

x/liquidfarming/module_test.go
@@ -1,0 +1,101 @@
+package liquidfarming_test
+
+import (
+	"encoding/binary"
+
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	chain "github.com/cosmosquad-labs/squad/v3/app"
+	"github.com/cosmosquad-labs/squad/v3/x/liquidfarming/keeper"
+	"github.com/cosmosquad-labs/squad/v3/x/liquidfarming/types"
+	liquiditytypes "github.com/cosmosquad-labs/squad/v3/x/liquidity/types"
+
+	_ "github.com/stretchr/testify/suite"
+)
+
+type ModuleTestSuite struct {
+	suite.Suite
+
+	app       *chain.App
+	ctx       sdk.Context
+	keeper    keeper.Keeper
+	querier   keeper.Querier
+	msgServer types.MsgServer
+}
+
+func TestModuleTestSuite(t *testing.T) {
+	suite.Run(t, new(ModuleTestSuite))
+}
+
+func (s *ModuleTestSuite) SetupTest() {
+	s.app = chain.Setup(false)
+	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
+	s.keeper = s.app.LiquidFarmingKeeper
+	s.querier = keeper.Querier{Keeper: s.keeper}
+	s.msgServer = keeper.NewMsgServerImpl(s.keeper)
+	s.ctx = s.ctx.WithBlockTime(time.Now()) // set to current time
+}
+
+//
+// Below are just shortcuts to frequently-used functions.
+//
+
+func (s *ModuleTestSuite) fundAddr(addr sdk.AccAddress, amt sdk.Coins) { //nolint
+	s.T().Helper()
+	err := s.app.BankKeeper.MintCoins(s.ctx, types.ModuleName, amt)
+	s.Require().NoError(err)
+	err = s.app.BankKeeper.SendCoinsFromModuleToAccount(s.ctx, types.ModuleName, addr, amt)
+	s.Require().NoError(err)
+}
+
+func (s *ModuleTestSuite) createPair(creator sdk.AccAddress, baseCoinDenom, quoteCoinDenom string) liquiditytypes.Pair {
+	s.T().Helper()
+	s.fundAddr(creator, s.app.LiquidityKeeper.GetPairCreationFee(s.ctx))
+	pair, err := s.app.LiquidityKeeper.CreatePair(s.ctx, liquiditytypes.NewMsgCreatePair(creator, baseCoinDenom, quoteCoinDenom))
+	s.Require().NoError(err)
+	return pair
+}
+
+// createPairWithLastPrice is a convenient method to create a pair with last price.
+// it is needed as farming plan doesn't distribute farming rewards if the last price is not set.
+func (s *ModuleTestSuite) createPairWithLastPrice(creator sdk.AccAddress, baseCoinDenom, quoteCoinDenom string, lastPrice sdk.Dec) liquiditytypes.Pair {
+	s.T().Helper()
+	pair := s.createPair(creator, baseCoinDenom, quoteCoinDenom)
+	pair.LastPrice = &lastPrice
+	s.app.LiquidityKeeper.SetPair(s.ctx, pair)
+	return pair
+}
+
+func (s *ModuleTestSuite) createPool(creator sdk.AccAddress, pairId uint64, depositCoins sdk.Coins) liquiditytypes.Pool {
+	s.T().Helper()
+	s.fundAddr(creator, s.app.LiquidityKeeper.GetPoolCreationFee(s.ctx).Add(depositCoins...))
+	pool, err := s.app.LiquidityKeeper.CreatePool(s.ctx, liquiditytypes.NewMsgCreatePool(creator, pairId, depositCoins))
+	s.Require().NoError(err)
+	return pool
+}
+
+func (s *ModuleTestSuite) createLiquidFarm(poolId uint64, minFarmAmt, minBidAmt sdk.Int, feeRate sdk.Dec) types.LiquidFarm { //nolint
+	s.T().Helper()
+	liquidFarm := types.NewLiquidFarm(poolId, minFarmAmt, minBidAmt, feeRate)
+	params := s.keeper.GetParams(s.ctx)
+	params.LiquidFarms = append(params.LiquidFarms, liquidFarm)
+	s.keeper.SetParams(s.ctx, params)
+	s.keeper.SetLiquidFarm(s.ctx, liquidFarm)
+	return liquidFarm
+}
+
+//
+// Below are helper functions to write test code easily
+//
+
+func (s *ModuleTestSuite) addr(addrNum int) sdk.AccAddress {
+	addr := make(sdk.AccAddress, 20)
+	binary.PutVarint(addr, int64(addrNum))
+	return addr
+}

crescent-network/crescent

(cherry picked from commit 0a1f273b11b23193ca259441c0520a71e6550c45)

x/liquidfarming/client/cli/tx.go
@@ -13,6 +13,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 
+	"github.com/crescent-network/crescent/v3/x/liquidfarming/keeper"
 	"github.com/crescent-network/crescent/v3/x/liquidfarming/types"
 )
 
@@ -33,6 +34,10 @@ func GetTxCmd() *cobra.Command {
 		NewPlaceBidCmd(),
 		NewRefundBidCmd(),
 	)
+
+	if keeper.EnableAdvanceAuction {
+		cmd.AddCommand(NewAdvanceAuctionCmd())
+	}
 
 	return cmd
 
@@ -266,6 +271,41 @@ $ %s tx %s refund-bid 1 1 --from mykey
 				poolId,
 				clientCtx.GetFromAddress().String(),
 			)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// NewAdvanceAuctionCmd implements the advance auction by 1 command handler.
+func NewAdvanceAuctionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "advance-auction",
+		Args:  cobra.NoArgs,
+		Short: "Advance auction by 1 to simulate rewards auction",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Advance auction by 1 to simulate rewards auction.
+This message is available for testing purpose and it can only be enabled when you build the binary with "make install-testing" command. 
+
+Example:
+$ %s tx %s advance-auction --from mykey
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			requesterAcc := clientCtx.GetFromAddress()
+
+			msg := types.NewMsgAdvanceAuction(requesterAcc)
 
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},

x/liquidfarming/module_test.go
@@ -1,0 +1,101 @@
+package liquidfarming_test
+
+import (
+	"encoding/binary"
+
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	chain "github.com/crescent-network/crescent/v3/app"
+	"github.com/crescent-network/crescent/v3/x/liquidfarming/keeper"
+	"github.com/crescent-network/crescent/v3/x/liquidfarming/types"
+	liquiditytypes "github.com/crescent-network/crescent/v3/x/liquidity/types"
+
+	_ "github.com/stretchr/testify/suite"
+)
+
+type ModuleTestSuite struct {
+	suite.Suite
+
+	app       *chain.App
+	ctx       sdk.Context
+	keeper    keeper.Keeper
+	querier   keeper.Querier
+	msgServer types.MsgServer
+}
+
+func TestModuleTestSuite(t *testing.T) {
+	suite.Run(t, new(ModuleTestSuite))
+}
+
+func (s *ModuleTestSuite) SetupTest() {
+	s.app = chain.Setup(false)
+	s.ctx = s.app.BaseApp.NewContext(false, tmproto.Header{})
+	s.keeper = s.app.LiquidFarmingKeeper
+	s.querier = keeper.Querier{Keeper: s.keeper}
+	s.msgServer = keeper.NewMsgServerImpl(s.keeper)
+	s.ctx = s.ctx.WithBlockTime(time.Now()) // set to current time
+}
+
+//
+// Below are just shortcuts to frequently-used functions.
+//
+
+func (s *ModuleTestSuite) fundAddr(addr sdk.AccAddress, amt sdk.Coins) { //nolint
+	s.T().Helper()
+	err := s.app.BankKeeper.MintCoins(s.ctx, types.ModuleName, amt)
+	s.Require().NoError(err)
+	err = s.app.BankKeeper.SendCoinsFromModuleToAccount(s.ctx, types.ModuleName, addr, amt)
+	s.Require().NoError(err)
+}
+
+func (s *ModuleTestSuite) createPair(creator sdk.AccAddress, baseCoinDenom, quoteCoinDenom string) liquiditytypes.Pair {
+	s.T().Helper()
+	s.fundAddr(creator, s.app.LiquidityKeeper.GetPairCreationFee(s.ctx))
+	pair, err := s.app.LiquidityKeeper.CreatePair(s.ctx, liquiditytypes.NewMsgCreatePair(creator, baseCoinDenom, quoteCoinDenom))
+	s.Require().NoError(err)
+	return pair
+}
+
+// createPairWithLastPrice is a convenient method to create a pair with last price.
+// it is needed as farming plan doesn't distribute farming rewards if the last price is not set.
+func (s *ModuleTestSuite) createPairWithLastPrice(creator sdk.AccAddress, baseCoinDenom, quoteCoinDenom string, lastPrice sdk.Dec) liquiditytypes.Pair {
+	s.T().Helper()
+	pair := s.createPair(creator, baseCoinDenom, quoteCoinDenom)
+	pair.LastPrice = &lastPrice
+	s.app.LiquidityKeeper.SetPair(s.ctx, pair)
+	return pair
+}
+
+func (s *ModuleTestSuite) createPool(creator sdk.AccAddress, pairId uint64, depositCoins sdk.Coins) liquiditytypes.Pool {
+	s.T().Helper()
+	s.fundAddr(creator, s.app.LiquidityKeeper.GetPoolCreationFee(s.ctx).Add(depositCoins...))
+	pool, err := s.app.LiquidityKeeper.CreatePool(s.ctx, liquiditytypes.NewMsgCreatePool(creator, pairId, depositCoins))
+	s.Require().NoError(err)
+	return pool
+}
+
+func (s *ModuleTestSuite) createLiquidFarm(poolId uint64, minFarmAmt, minBidAmt sdk.Int, feeRate sdk.Dec) types.LiquidFarm { //nolint
+	s.T().Helper()
+	liquidFarm := types.NewLiquidFarm(poolId, minFarmAmt, minBidAmt, feeRate)
+	params := s.keeper.GetParams(s.ctx)
+	params.LiquidFarms = append(params.LiquidFarms, liquidFarm)
+	s.keeper.SetParams(s.ctx, params)
+	s.keeper.SetLiquidFarm(s.ctx, liquidFarm)
+	return liquidFarm
+}
+
+//
+// Below are helper functions to write test code easily
+//
+
+func (s *ModuleTestSuite) addr(addrNum int) sdk.AccAddress {
+	addr := make(sdk.AccAddress, 20)
+	binary.PutVarint(addr, int64(addrNum))
+	return addr
+}