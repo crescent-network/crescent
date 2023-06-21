ccea94b78af009cbca27e7791fcd64ae74e16257
crypin <crypin@crescent.foundation>
2023-06-21 13:18:42 +0900

    feat!: add ante msg filter for mixed batch msg, deprecated module msgs

--- app/ante/msg_ante_test.go
+++ app/ante/msg_ante_test.go
@@ -11,6 +11,9 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	claimtypes "github.com/crescent-network/crescent/v5/x/claim/types"
+	exchangetypes "github.com/crescent-network/crescent/v5/x/exchange/types"
+	farmingtypes "github.com/crescent-network/crescent/v5/x/farming/types"
+	liquiditytypes "github.com/crescent-network/crescent/v5/x/liquidity/types"
 )
 
 type runTxMode uint8
@@ -30,7 +33,6 @@ func (suite *AnteTestSuite) TestAnteHandlerSubmitProposalMsg() {
 	feeAmount := testdata.NewTestFeeAmount()
 	gasLimit := testdata.NewTestGasLimit()
 	acc := accounts[0].acc.GetAddress()
-	//accStr := acc.String()
 
 	// Variable data per test case
 	var (
@@ -322,6 +324,53 @@ func (suite *AnteTestSuite) TestAnteHandlerDeprecatedMsg() {
 			false,
 			fmt.Errorf("/crescent.claim.v1beta1.MsgClaim is deprecated msg type"),
 		},
+		{
+			"deprecated msg - farming",
+			func() {
+				msg := &farmingtypes.MsgStake{
+					Farmer:       accStr,
+					StakingCoins: sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1))),
+				}
+				msgs = []sdk.Msg{msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{0}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("/crescent.farming.v1beta1.MsgStake is deprecated msg type"),
+		},
+		{
+			"deprecated msg - liquidity",
+			func() {
+				msg := &liquiditytypes.MsgCreatePair{
+					Creator:        accStr,
+					BaseCoinDenom:  "abc",
+					QuoteCoinDenom: "stake",
+				}
+				msgs = []sdk.Msg{msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{0}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("/crescent.liquidity.v1beta1.MsgCreatePair is deprecated msg type"),
+		},
+		{
+			"not deprecated msg",
+			func() {
+				msg := &exchangetypes.MsgCreateMarket{
+					Sender:     accStr,
+					BaseDenom:  "abc",
+					QuoteDenom: "stake",
+				}
+				msgs = []sdk.Msg{msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{1}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -342,7 +391,6 @@ func (suite *AnteTestSuite) TestDoubleNestedAuthzMsg() {
 	feeAmount := testdata.NewTestFeeAmount()
 	gasLimit := testdata.NewTestGasLimit()
 	acc := accounts[0].acc.GetAddress()
-	//accStr := acc.String()
 
 	// Variable data per test case
 	var (
@@ -392,6 +440,175 @@ func (suite *AnteTestSuite) TestDoubleNestedAuthzMsg() {
 			runTxModeCheck,
 			false,
 			fmt.Errorf("double nested /cosmos.authz.v1beta1.MsgExec is not allowed"),
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
+			suite.txBuilder = suite.clientCtx.TxConfig.NewTxBuilder()
+			tc.malleate()
+
+			suite.RunTestCase(privs, msgs, feeAmount, gasLimit, accNums, accSeqs, suite.ctx.ChainID(), tc)
+		})
+	}
+}
+
+func (suite *AnteTestSuite) TestMixedBatchMsg() {
+	suite.SetupTest(false) // reset
+
+	// Same data for every test cases
+	accounts := suite.CreateTestAccounts(2)
+	feeAmount := testdata.NewTestFeeAmount()
+	gasLimit := testdata.NewTestGasLimit()
+	acc := accounts[0].acc.GetAddress()
+
+	// Variable data per test case
+	var (
+		accNums []uint64
+		msgs    []sdk.Msg
+		privs   []cryptotypes.PrivKey
+		accSeqs []uint64
+	)
+
+	msg := testdata.NewTestMsg(acc)
+	batchMsg := exchangetypes.NewMsgPlaceBatchLimitOrder(acc, 1, true, sdk.ZeroDec(), sdk.ZeroInt(), 0)
+	authzMsg := authz.NewMsgExec(acc, []sdk.Msg{batchMsg, msg})
+	authzMsg2 := authz.NewMsgExec(acc, []sdk.Msg{batchMsg})
+	authzMsg3 := authz.NewMsgExec(acc, []sdk.Msg{batchMsg, batchMsg})
+	//authzMsgNested := authz.NewMsgExec(acc, []sdk.Msg{&authzMsg})
+
+	testCases := []TestCase{
+		{
+			"only batch msg",
+			func() {
+
+				msgs = []sdk.Msg{batchMsg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{0}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
+		},
+		{
+			"only batch msgs",
+			func() {
+
+				msgs = []sdk.Msg{batchMsg, batchMsg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{1}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
+		},
+		{
+			"mixed batch msg with multi msg",
+			func() {
+
+				msgs = []sdk.Msg{batchMsg, msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"mixed batch msg with multi msg - check tx",
+			func() {
+
+				msgs = []sdk.Msg{batchMsg, msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeCheck,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"mixed batch msg with multi msg",
+			func() {
+
+				msgs = []sdk.Msg{msg, batchMsg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"mixed batch msg with authz msg",
+			func() {
+
+				msgs = []sdk.Msg{msg, &authzMsg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"mixed batch msg with authz msg - 2",
+			func() {
+
+				msgs = []sdk.Msg{&authzMsg, msg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"mixed batch msg with authz msg - 3",
+			func() {
+
+				msgs = []sdk.Msg{msg, &authzMsg2}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			false,
+			fmt.Errorf("cannot mix batch msg and regular msg in one tx"),
+		},
+		{
+			"authz only batch msg",
+			func() {
+
+				msgs = []sdk.Msg{&authzMsg2}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{2}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
+		},
+		{
+			"authz only batch msg - 2",
+			func() {
+
+				msgs = []sdk.Msg{&authzMsg3}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{3}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
+		},
+		{
+			"authz only batch msg - 3",
+			func() {
+
+				msgs = []sdk.Msg{&authzMsg3, batchMsg}
+
+				privs, accNums, accSeqs = []cryptotypes.PrivKey{accounts[0].priv}, []uint64{0}, []uint64{4}
+			},
+			runTxModeDeliver,
+			true,
+			nil,
 		},
 	}
 
