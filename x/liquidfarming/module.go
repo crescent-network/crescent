abe1210ed86150555429aca6509a921779a67ccb
King <king@crescent.foundation>
2023-06-21 22:07:29 +0900

    fix: revert liquidfarming consensus version to 1

--- x/liquidfarming/module.go
+++ x/liquidfarming/module.go
@@ -162,7 +162,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 2 }
+func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
