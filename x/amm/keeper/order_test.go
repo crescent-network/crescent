b8e68310e0b83870ae1ab245e7194210c1035bad
King <king@crescent.foundation>
2023-06-28 19:05:44 +0900

    test: add order benchmark test

--- x/amm/keeper/order_test.go
+++ x/amm/keeper/order_test.go
@@ -2,13 +2,85 @@ package keeper_test
 
 import (
 	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
+	chain "github.com/crescent-network/crescent/v5/app"
 	utils "github.com/crescent-network/crescent/v5/types"
 	"github.com/crescent-network/crescent/v5/x/amm/types"
 	exchangetypes "github.com/crescent-network/crescent/v5/x/exchange/types"
 )
+
+func BenchmarkPlaceMarketOrder(b *testing.B) {
+	app := chain.Setup(false)
+	ctx := app.NewContext(false, tmproto.Header{
+		Height: 0,
+		Time:   utils.ParseTime("2023-01-01T00:00:00Z"),
+	})
+
+	// Create market.
+	creatorAddr := utils.TestAddress(1)
+	require.NoError(b, chain.FundAccount(app.BankKeeper, ctx, creatorAddr, enoughCoins))
+	market, err := app.ExchangeKeeper.CreateMarket(ctx, creatorAddr, "ucre", "uusd")
+	require.NoError(b, err)
+	// Create pool and add liquidity
+	pool, err := app.AMMKeeper.CreatePool(ctx, creatorAddr, market.Id, utils.ParseDec("5"))
+	require.NoError(b, err)
+	_, _, _, err = app.AMMKeeper.AddLiquidity(
+		ctx, creatorAddr, creatorAddr, pool.Id,
+		exchangetypes.PriceAtTick(types.MinTick), exchangetypes.PriceAtTick(types.MaxTick),
+		utils.ParseCoins("10_000000ucre,50_000000uusd"))
+	require.NoError(b, err)
+
+	// Prepare orderer.
+	ordererAddr := utils.TestAddress(2)
+	require.NoError(b, chain.FundAccount(app.BankKeeper, ctx, ordererAddr, enoughCoins))
+
+	// Pre-run.
+	isBuy := false
+	for i := 0; i < 100; i++ {
+		hdr := ctx.BlockHeader()
+		hdr.Height++
+		hdr.Time = hdr.Time.Add(5 * time.Second)
+		app.BeginBlock(abci.RequestBeginBlock{Header: hdr})
+		ctx = app.NewContext(false, hdr)
+
+		_, _, _, _, err := app.ExchangeKeeper.PlaceMarketOrder(
+			ctx, market.Id, ordererAddr, isBuy, sdk.NewInt(10_000000))
+		require.NoError(b, err)
+		isBuy = !isBuy
+
+		app.EndBlock(abci.RequestEndBlock{Height: ctx.BlockHeight()})
+		app.Commit()
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		hdr := ctx.BlockHeader()
+		hdr.Height++
+		hdr.Time = hdr.Time.Add(5 * time.Second)
+		app.BeginBlock(abci.RequestBeginBlock{Header: hdr})
+		ctx = app.NewContext(false, hdr)
+		b.StartTimer()
+
+		_, _, _, _, err := app.ExchangeKeeper.PlaceMarketOrder(
+			ctx, market.Id, ordererAddr, isBuy, sdk.NewInt(10_000000))
+		require.NoError(b, err)
+		isBuy = !isBuy
+
+		b.StopTimer()
+
+		app.EndBlock(abci.RequestEndBlock{Height: ctx.BlockHeight()})
+		app.Commit()
+	}
+}
 
 func (s *KeeperTestSuite) TestOrderGas() {
 	currentPrice := utils.ParseDec("67.855")
