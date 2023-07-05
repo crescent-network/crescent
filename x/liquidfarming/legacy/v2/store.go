92a564c82fa3b3c4d8d8b9127e664dedfc006e6d
King <king@crescent.foundation>
2023-07-05 17:14:19 +0900

    fix: handle legacy end time properly

--- x/liquidfarming/legacy/v2/store.go
+++ x/liquidfarming/legacy/v2/store.go
@@ -3,12 +3,15 @@ package v2
 import (
 	"bytes"
 
+	gogotypes "github.com/gogo/protobuf/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	v1 "github.com/crescent-network/crescent/v5/x/liquidfarming/legacy/v1"
 )
 
-func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) error {
 	store := ctx.KVStore(storeKey)
 	// Delete all previous states except for LastRewardsAuctionEndTime.
 	iter := store.Iterator(nil, nil)
@@ -19,5 +22,19 @@ func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 		}
 		store.Delete(iter.Key())
 	}
+	// Migrate NextRewardsAuctionEndTime.
+	bz := store.Get(v1.LastRewardsAuctionEndTimeKey)
+	if bz != nil {
+		var ts gogotypes.Timestamp
+		if err := cdc.Unmarshal(bz, &ts); err != nil {
+			return err
+		}
+		endTime, err := gogotypes.TimestampFromProto(&ts)
+		if err != nil {
+			return err
+		}
+		store.Set(NextRewardsAuctionEndTimeKey, sdk.FormatTimeBytes(endTime))
+	}
+	// TODO: Delete old parameters
 	return nil
 }
--- x/liquidfarming/legacy/v2/store_test.go
+++ x/liquidfarming/legacy/v2/store_test.go
@@ -3,11 +3,13 @@ package v2_test
 import (
 	"testing"
 
+	gogotypes "github.com/gogo/protobuf/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	chain "github.com/crescent-network/crescent/v5/app"
 	utils "github.com/crescent-network/crescent/v5/types"
 	v1liquidfarming "github.com/crescent-network/crescent/v5/x/liquidfarming/legacy/v1"
 	v2liquidfarming "github.com/crescent-network/crescent/v5/x/liquidfarming/legacy/v2"
@@ -20,8 +22,12 @@ func TestMigrateStore(t *testing.T) {
 	ctx := testutil.DefaultContext(storeKey, tKey)
 	store := ctx.KVStore(storeKey)
 
-	endTimeBytes := sdk.FormatTimeBytes(utils.ParseTime("2023-01-01T08:00:00Z"))
-	store.Set(v1liquidfarming.LastRewardsAuctionEndTimeKey, endTimeBytes)
-	require.NoError(t, v2liquidfarming.MigrateStore(ctx, storeKey))
-	require.Equal(t, endTimeBytes, store.Get(v2liquidfarming.NextRewardsAuctionEndTimeKey))
+	endTime := utils.ParseTime("2023-01-01T08:00:00Z")
+	ts, err := gogotypes.TimestampProto(endTime)
+	require.NoError(t, err)
+	cdc := chain.MakeTestEncodingConfig().Marshaler
+	bz := cdc.MustMarshal(ts)
+	store.Set(v1liquidfarming.LastRewardsAuctionEndTimeKey, bz)
+	require.NoError(t, v2liquidfarming.MigrateStore(ctx, storeKey, cdc))
+	require.Equal(t, sdk.FormatTimeBytes(endTime), store.Get(v2liquidfarming.NextRewardsAuctionEndTimeKey))
 }
