26330d90eeaacb6496012170762d6e194d692339
King <king@crescent.foundation>
2023-08-02 21:21:05 +0900

    test: add scenario tests

--- x/amm/keeper/position_test.go
+++ x/amm/keeper/position_test.go
@@ -2,6 +2,7 @@ package keeper_test
 
 import (
 	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -156,6 +157,229 @@ func (s *KeeperTestSuite) TestSmallPosition() {
 		lpAddr, pool1.Id, utils.ParseDec("0.5"), utils.ParseDec("1.5"),
 		utils.ParseCoins("7ucre,10uusd"))
 	s.AssertEqual(sdk.NewInt(34), liquidity)
+	// TODO: check pool orders
 
-	// TODO: write more code
+	_, pool2 := s.CreateMarketAndPool("uusd", "ucre", utils.ParseDec("1"))
+	_, liquidity, _ = s.AddLiquidity(
+		lpAddr, pool2.Id, utils.ParseDec("0.99999"), utils.ParseDec("1.0001"),
+		utils.ParseCoins("100uusd,10ucre"))
+	s.AssertEqual(sdk.NewInt(1999994), liquidity)
+	// TODO: check pool orders
+
+	s.FundAccount(lpAddr, utils.ParseCoins("1ufoo,1ubar"))
+	_, pool3 := s.CreateMarketAndPool("ufoo", "ubar", utils.ParseDec("1000"))
+	_, _, _, err := s.keeper.AddLiquidity(
+		s.Ctx, lpAddr, lpAddr, pool3.Id, utils.ParseDec("900"), utils.ParseDec("1100"),
+		utils.ParseCoins("1ufoo,1ubar"))
+	s.Require().EqualError(err, "minted liquidity is zero: insufficient funds")
+}
+
+func (s *KeeperTestSuite) TestBigPosition() {
+	s.keeper.SetDefaultTickSpacing(s.Ctx, 1)
+	_, pool := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("1"))
+
+	lpAddr := s.FundedAccount(1, enoughCoins)
+	_, liquidity, _ := s.AddLiquidity(
+		lpAddr, pool.Id, utils.ParseDec("0.99999"), utils.ParseDec("1.0001"),
+		utils.ParseCoins("9999225064335ucre,1000000000000uusd"))
+	s.AssertEqual(sdk.NewInt(199999499998770009), liquidity)
+}
+
+func (s *KeeperTestSuite) TestSingleSidePosition() {
+	s.keeper.SetDefaultTickSpacing(s.Ctx, 1)
+	_, pool1 := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("10"))
+
+	lpAddr := s.FundedAccount(1, enoughCoins)
+	_, liquidity, _ := s.AddLiquidity(
+		lpAddr, pool1.Id, utils.ParseDec("10"), utils.ParseDec("20"),
+		utils.ParseCoins("1000000000ucre"))
+	s.AssertEqual(sdk.NewInt(10796691275), liquidity)
+
+	_, pool2 := s.CreateMarketAndPool("uusd", "ucre", utils.ParseDec("20"))
+	_, liquidity, _ = s.AddLiquidity(
+		lpAddr, pool2.Id, utils.ParseDec("10"), utils.ParseDec("20"),
+		utils.ParseCoins("1000000000ucre"))
+	s.AssertEqual(sdk.NewInt(763441361), liquidity)
+}
+
+func (s *KeeperTestSuite) TestSamePositions() {
+	s.keeper.SetDefaultTickSpacing(s.Ctx, 1)
+	market, pool := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("5"))
+
+	lpAddr1 := s.FundedAccount(1, enoughCoins)
+	position1, _, _ := s.AddLiquidity(
+		lpAddr1, pool.Id, utils.ParseDec("2"), utils.ParseDec("7"),
+		utils.ParseCoins("84259598ucre,1000000000uusd"))
+	lpAddr2 := s.FundedAccount(2, enoughCoins)
+	position2, _, _ := s.AddLiquidity(
+		lpAddr2, pool.Id, utils.ParseDec("2"), utils.ParseDec("7"),
+		utils.ParseCoins("84259598ucre,1000000000uusd"))
+	lpAddr3 := s.FundedAccount(3, enoughCoins)
+	position3, _, _ := s.AddLiquidity(
+		lpAddr3, pool.Id, utils.ParseDec("2"), utils.ParseDec("7"),
+		utils.ParseCoins("84259598ucre,1000000000uusd"))
+	s.AssertEqual(sdk.NewInt(1216760513), position1.Liquidity)
+	s.AssertEqual(sdk.NewInt(1216760513), position2.Liquidity)
+	s.AssertEqual(sdk.NewInt(1216760513), position3.Liquidity)
+
+	ordererAddr := s.FundedAccount(2, enoughCoins)
+	s.PlaceMarketOrder(market.Id, ordererAddr, false, sdk.NewDec(100000000))
+
+	fee, _ := s.CollectibleCoins(position1.Id)
+	s.AssertEqual(utils.ParseCoins("234609uusd"), fee)
+	fee, _ = s.CollectibleCoins(position2.Id)
+	s.AssertEqual(utils.ParseCoins("234609uusd"), fee)
+	fee, _ = s.CollectibleCoins(position3.Id)
+	s.AssertEqual(utils.ParseCoins("234609uusd"), fee)
+
+	_, amt := s.RemoveLiquidity(lpAddr1, position1.Id, position1.Liquidity)
+	s.AssertEqual(utils.ParseCoins("117592576ucre,842955162uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr2, position2.Id, position2.Liquidity)
+	s.AssertEqual(utils.ParseCoins("117592576ucre,842955162uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr3, position3.Id, position3.Liquidity)
+	s.AssertEqual(utils.ParseCoins("117592576ucre,842955162uusd"), amt)
+}
+
+func (s *KeeperTestSuite) TestSamePositions2() {
+	market, pool := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("500"))
+
+	lpAddr1 := s.FundedAccount(1, enoughCoins)
+	position1, _, _ := s.AddLiquidity(
+		lpAddr1, pool.Id, utils.ParseDec("400"), utils.ParseDec("625"),
+		utils.ParseCoins("2000000000ucre,1000000000000uusd"))
+	lpAddr2 := s.FundedAccount(2, enoughCoins)
+	position2, _, _ := s.AddLiquidity(
+		lpAddr2, pool.Id, utils.ParseDec("400"), utils.ParseDec("625"),
+		utils.ParseCoins("200000000ucre,100000000000uusd"))
+	s.AssertEqual(sdk.NewInt(423606797749), position1.Liquidity)
+	s.AssertEqual(sdk.NewInt(42360679774), position2.Liquidity)
+
+	ordererAddr := s.FundedAccount(2, enoughCoins)
+	s.PlaceMarketOrder(market.Id, ordererAddr, true, sdk.NewDec(200000000))
+
+	fee, _ := s.CollectibleCoins(position1.Id)
+	s.AssertEqual(utils.ParseCoins("272718ucre,46039469uusd"), fee)
+	fee, _ = s.CollectibleCoins(position2.Id)
+	s.AssertEqual(utils.ParseCoins("27271ucre,4603946uusd"), fee)
+
+	// Use cache context to discard the operation results.
+	cacheCtx, _ := s.Ctx.CacheContext()
+	_, amt, err := s.keeper.RemoveLiquidity(cacheCtx, lpAddr1, lpAddr1, position1.Id, position1.Liquidity)
+	s.Require().NoError(err)
+	s.AssertEqual(utils.ParseCoins("1818181818ucre,1091790048475uusd"), amt)
+	_, amt, err = s.keeper.RemoveLiquidity(cacheCtx, lpAddr2, lpAddr2, position2.Id, position2.Liquidity)
+	s.Require().NoError(err)
+	s.AssertEqual(utils.ParseCoins("181818181ucre,109179004845uusd"), amt)
+
+	_, amt = s.RemoveLiquidity(lpAddr2, position2.Id, position2.Liquidity)
+	s.AssertEqual(utils.ParseCoins("181818181ucre,109179004845uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr1, position1.Id, position1.Liquidity)
+	s.AssertEqual(utils.ParseCoins("1818181818ucre,1091790048475uusd"), amt)
+}
+
+func (s *KeeperTestSuite) TestSymmetricPositions() {
+	_, pool := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("100"))
+
+	lpAddr1 := s.FundedAccount(1, enoughCoins)
+	_, liquidity, amt := s.AddLiquidity(
+		lpAddr1, pool.Id, utils.ParseDec("50"), utils.ParseDec("200"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	s.AssertEqual(sdk.NewInt(34142135623), liquidity)
+	s.AssertEqual(utils.ParseCoins("1000000000ucre,99999999998uusd"), amt)
+
+	lpAddr2 := s.FundedAccount(2, enoughCoins)
+	_, liquidity, amt = s.AddLiquidity(
+		lpAddr2, pool.Id, utils.ParseDec("20"), utils.ParseDec("500"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	s.AssertEqual(sdk.NewInt(18090169943), liquidity)
+	s.AssertEqual(utils.ParseCoins("1000000000ucre,99999999996uusd"), amt)
+}
+
+func (s *KeeperTestSuite) TestIndependentRemoveLiquidity() {
+	s.keeper.SetDefaultTickSpacing(s.Ctx, 1)
+	_, pool := s.CreateMarketAndPool("ucre", "uusd", utils.ParseDec("100"))
+
+	lpAddr1 := s.FundedAccount(1, enoughCoins)
+	position1, _, _ := s.AddLiquidity(
+		lpAddr1, pool.Id, utils.ParseDec("50"), utils.ParseDec("200"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	lpAddr2 := s.FundedAccount(2, enoughCoins)
+	position2, _, _ := s.AddLiquidity(
+		lpAddr2, pool.Id, utils.ParseDec("50"), utils.ParseDec("200"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	lpAddr3 := s.FundedAccount(3, enoughCoins)
+	position3, _, _ := s.AddLiquidity(
+		lpAddr3, pool.Id, utils.ParseDec("20"), utils.ParseDec("500"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	lpAddr4 := s.FundedAccount(4, enoughCoins)
+	position4, _, _ := s.AddLiquidity(
+		lpAddr4, pool.Id, utils.ParseDec("20"), utils.ParseDec("500"),
+		utils.ParseCoins("1000000000ucre,100000000000uusd"))
+	s.AssertEqual(sdk.NewInt(34142135623), position1.Liquidity)
+	s.AssertEqual(sdk.NewInt(34142135623), position2.Liquidity)
+	s.AssertEqual(sdk.NewInt(18090169943), position3.Liquidity)
+	s.AssertEqual(sdk.NewInt(18090169943), position4.Liquidity)
+
+	_, amt := s.RemoveLiquidity(lpAddr3, position3.Id, position3.Liquidity)
+	s.AssertEqual(utils.ParseCoins("999999999ucre,99999999995uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr1, position1.Id, position1.Liquidity)
+	s.AssertEqual(utils.ParseCoins("999999999ucre,99999999997uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr2, position2.Id, position2.Liquidity)
+	s.AssertEqual(utils.ParseCoins("999999999ucre,99999999997uusd"), amt)
+	_, amt = s.RemoveLiquidity(lpAddr4, position4.Id, position4.Liquidity)
+	s.AssertEqual(utils.ParseCoins("999999999ucre,99999999995uusd"), amt)
+}
+
+func (s *KeeperTestSuite) TestInitialPoolPriceDifference() {
+	market := s.CreateMarket("ucre", "uusd")
+
+	ordererAddr1 := s.FundedAccount(1, enoughCoins)
+	ordererAddr2 := s.FundedAccount(2, enoughCoins)
+	s.PlaceLimitOrder(market.Id, ordererAddr1, false, utils.ParseDec("10"), sdk.NewDec(10000), time.Hour)
+	_, buyOrder, _ := s.PlaceLimitOrder(market.Id, ordererAddr2, true, utils.ParseDec("10"), sdk.NewDec(1000000), time.Hour)
+
+	marketState := s.App.ExchangeKeeper.MustGetMarketState(s.Ctx, market.Id)
+	s.Require().NotNil(marketState.LastPrice)
+	s.AssertEqual(utils.ParseDec("10"), *marketState.LastPrice)
+
+	buyOrder = s.App.ExchangeKeeper.MustGetOrder(s.Ctx, buyOrder.Id)
+	s.AssertEqual(sdk.NewDec(990000), buyOrder.OpenQuantity)
+
+	pool := s.CreatePool(market.Id, utils.ParseDec("1"))
+	lpAddr := s.FundedAccount(3, enoughCoins)
+	position, _, _ := s.AddLiquidity(
+		lpAddr, pool.Id, utils.ParseDec("0.1"), utils.ParseDec("10"),
+		utils.ParseCoins("1000ucre,1000uusd"))
+	s.AssertEqual(sdk.NewInt(1462), position.Liquidity)
+
+	s.NextBlock() // Run matching at batch
+
+	// TODO: write the rest of test after fixing bug in batch matching
+}
+
+func (s *KeeperTestSuite) TestInitialPoolPriceDifference2() {
+	market := s.CreateMarket("ucre", "uusd")
+
+	ordererAddr1 := s.FundedAccount(1, enoughCoins)
+	ordererAddr2 := s.FundedAccount(2, enoughCoins)
+	_, sellOrder, _ := s.PlaceLimitOrder(market.Id, ordererAddr1, false, utils.ParseDec("1"), sdk.NewDec(1000000), time.Hour)
+	s.PlaceLimitOrder(market.Id, ordererAddr2, true, utils.ParseDec("1"), sdk.NewDec(10000), time.Hour)
+
+	marketState := s.App.ExchangeKeeper.MustGetMarketState(s.Ctx, market.Id)
+	s.Require().NotNil(marketState.LastPrice)
+	s.AssertEqual(utils.ParseDec("1"), *marketState.LastPrice)
+
+	sellOrder = s.App.ExchangeKeeper.MustGetOrder(s.Ctx, sellOrder.Id)
+	s.AssertEqual(sdk.NewDec(990000), sellOrder.OpenQuantity)
+
+	pool := s.CreatePool(market.Id, utils.ParseDec("10"))
+	lpAddr := s.FundedAccount(3, enoughCoins)
+	position, _, _ := s.AddLiquidity(
+		lpAddr, pool.Id, utils.ParseDec("1"), utils.ParseDec("100"),
+		utils.ParseCoins("100ucre,1000uusd"))
+	s.AssertEqual(sdk.NewInt(462), position.Liquidity)
+
+	s.NextBlock() // Run matching at batch
+
+	// TODO: write the rest of test after fixing bug in batch matching
 }
